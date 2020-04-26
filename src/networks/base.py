13bd808017635400b21f99cca6e0adfc31c43945
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2020-04-26 19:10:09 +0200

    changed letters model

--- src/networks/base.py
+++ src/networks/base.py
@@ -175,6 +175,8 @@ class LetterEncoder(nn.Module):
         self.lat_to_letters = nn.Sequential(
             ResidualBlock(1, letter_channels * letter_bits, None, 1, 1, 0, nn.Conv1d),
             ResidualBlock(letter_channels * letter_bits, letter_channels * letter_bits, None, 1, 1, 0, nn.Conv1d),
+            ResidualBlock(letter_channels * letter_bits, letter_channels * letter_bits, None, 1, 1, 0, nn.Conv1d),
+            ResidualBlock(letter_channels * letter_bits, letter_channels * letter_bits, None, 1, 1, 0, nn.Conv1d),
         )
 
     def forward(self, lat):
@@ -193,6 +195,8 @@ class LetterDecoder(nn.Module):
         self.letter_channels = letter_channels
         self.letter_bits = letter_bits
         self.letters_to_lat = nn.Sequential(
+            ResidualBlock(letter_channels * letter_bits, letter_channels * letter_bits, None, 1, 1, 0, nn.Conv1d),
+            ResidualBlock(letter_channels * letter_bits, letter_channels * letter_bits, None, 1, 1, 0, nn.Conv1d),
             ResidualBlock(letter_channels * letter_bits, letter_channels * letter_bits, None, 1, 1, 0, nn.Conv1d),
             ResidualBlock(letter_channels * letter_bits, 1, None, 1, 1, 0, nn.Conv1d),
         )
