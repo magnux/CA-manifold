f8cedfa6a38d14a1eee24213806cf5b038fc4efd
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2020-05-12 23:06:03 +0200

    improved test

--- test_cae.py
+++ test_cae.py
@@ -2,6 +2,7 @@
 import time
 import numpy as np
 import torch
+import torch.nn.functional as F
 import torchvision
 import argparse
 from tqdm import trange
@@ -34,8 +35,6 @@ n_workers = config['training']['n_workers']
 # Inputs
 trainset = get_dataset(name=config['data']['name'], type=config['data']['type'],
                        data_dir=config['data']['train_dir'], size=config['data']['image_size'])
-trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size,
-                                          shuffle=True, num_workers=n_workers, drop_last=True)
 
 # Networks
 networks_dict = {
@@ -58,33 +57,30 @@ if letter_encoding:
 model_manager.print()
 
 
-def get_inputs(trainiter, batch_size, device):
-    next_inputs = next(trainiter, None)
-    if trainiter is None or next_inputs is None:
-        trainiter = iter(trainloader)
-        next_inputs = next(trainiter, None)
-    images, labels = next_inputs
-    images, labels = images[:batch_size, ...], labels[:batch_size, ...]
-    images, labels = images.to(device), labels.to(device)
-    return images, labels, trainiter
+def forward_pass(images, init_seed=None):
+    lat_enc, _, _ = encoder(images)
+    if letter_encoding:
+        letters = letter_encoder(lat_enc)
+        lat_dec = letter_decoder(letters)
+    else:
+        lat_dec = lat_enc
+    images_dec, out_embs, _ = decoder(lat_dec, init_seed)
+    out_embs = torch.stack(out_embs, 0)
+    return images_dec, out_embs
 
 
-images_test, labels_test, trainiter = get_inputs(iter(trainloader), batch_size, device)
-
-if use_sample_pool:
-    n_slots = len(trainset) * 16
-    target = []
-    for _ in range((n_slots // batch_size) + 1):
-        images, _, trainiter = get_inputs(trainiter, batch_size, torch.device('cpu'))
-        target.append(images)
-    target = torch.cat(target, dim=0)[:n_slots, ...].numpy()
-    seed = ca_seed(n_slots, n_filter, image_size, torch.device('cpu')).numpy()
-    sample_pool = SamplePool(target=target, init=seed)
-    frac_size = batch_size // 16
-    frac_seed = ca_seed(frac_size, n_filter, image_size, torch.device('cpu')).numpy()
+def get_inputs(idxs, device):
+    images = []
+    labels = []
+    for idx in idxs:
+        image, label = trainset[idx]
+        images.append(image)
+        labels.append(label)
+    images = torch.stack(images, 0).to(device)
+    return images, labels
 
 
-def save_imgs(images, out_dir, tag):
+def save_imgs(images, out_dir, tag, make_video=False):
     if not os.path.exists(out_dir):
         os.makedirs(out_dir)
     images_file = os.path.join(out_dir, '%s.png' % tag)
@@ -92,36 +88,98 @@ def save_imgs(images, out_dir, tag):
     images = (images * 0.5) + 0.5
     torchvision.utils.save_image(images, images_file, nrow=8)  # imgs.size(0))
 
-    if images.size(1) == 4:
-        alpha = images[:, 3:4, :, :].clamp(0.0, 1.0)
-        images = images[:, :3, :, :] * alpha
-        images = ((1.0 - alpha) + images).clamp(0.0, 1.0)
+    if make_video:
+        if images.size(1) == 4:
+            alpha = images[:, 3:4, :, :].clamp(0.0, 1.0)
+            images = images[:, :3, :, :] * alpha
+            images = ((1.0 - alpha) + images).clamp(0.0, 1.0)
 
-    images = images * 255
+        images = images * 255
 
-    h, w = images.size()[2:]
-    video_file = os.path.join(out_dir, '%s.mp4' % tag)
-    video_writer = FFMPEG_VideoWriter(size=(w, h), filename=video_file, fps=images.size(0) / 8)
-    for i in range(images.size(0)):
-        video_writer.write_frame(np.uint8(images[i, ...].permute(1, 2, 0).cpu().numpy()))
-    for _ in range(images.size(0) // 4):
-        video_writer.write_frame(np.uint8(images[-1, ...].permute(1, 2, 0).cpu().numpy()))
-    video_writer.close()
+        h, w = images.size()[2:]
+        video_file = os.path.join(out_dir, '%s.mp4' % tag)
+        video_writer = FFMPEG_VideoWriter(size=(w, h), filename=video_file, fps=images.size(0) / 8)
+        for i in range(images.size(0)):
+            video_writer.write_frame(np.uint8(images[i, ...].permute(1, 2, 0).cpu().numpy()))
+        for _ in range(images.size(0) // 4):
+            video_writer.write_frame(np.uint8(images[-1, ...].permute(1, 2, 0).cpu().numpy()))
+        video_writer.close()
 
 
-its = [i for i in range(64)]
-# post_its = [2 ** i for i in range(6, 11)]
-idxs = [1, 3, 5, 7]
 with torch.no_grad():
-    model_manager.set_n_calls('decoder', 64)
-    lat_enc, _, _ = encoder(images_test)
-    if letter_encoding:
-        letters = letter_encoder(lat_enc)
-        lat_dec = letter_decoder(letters)
-    else:
-        lat_dec = lat_enc
-    images_dec, out_embs, _ = decoder(lat_dec)
-    out_embs = torch.stack(out_embs, 0)
-    for idx in idxs:
-        save_imgs(out_embs[its, idx, :config['data']['channels'], :, :], os.path.join(config['training']['out_dir'], 'test'), '%d_its' % idx)
-        # save_imgs(out_embs[its, idx, :config['data']['channels'], :, :], os.path.join(config['training']['out_dir'], 'test'), '%d_post_its' % idx)
+    print('Computing Error...')
+    t = trange((len(trainset) // batch_size) + 1)
+    losses = []
+    for batch in t:
+        idxs = torch.arange(batch * batch_size, min((batch + 1) * batch_size, len(trainset)))
+        images, labels = get_inputs(idxs, device)
+        images_dec, out_embs = forward_pass(images)
+        loss = F.mse_loss(images_dec, images, reduction='none').mean(dim=(1, 2, 3))
+        losses.append(loss)
+    losses = torch.cat(losses, 0)
+    print('MSE: ', losses.mean())
+
+    l_sort = torch.argsort(losses)
+    sorted_idxs = torch.arange(len(trainset))[l_sort]
+
+    print('Plotting Best CAs...')
+    images, labels = get_inputs(sorted_idxs[:batch_size], device)
+
+    images_dec, out_embs = forward_pass(images)
+    save_imgs(images_dec, os.path.join(config['training']['out_dir'], 'test', 'best'), 'all')
+    for i in range(batch_size):
+        save_imgs(out_embs[:, i, :config['data']['channels'], :, :], os.path.join(config['training']['out_dir'], 'test', 'best'), '%d' % i, True)
+
+    print('Plotting Worst CAs...')
+    images, labels = get_inputs(sorted_idxs[-batch_size:], device)
+
+    images_dec, out_embs = forward_pass(images)
+    save_imgs(images_dec, os.path.join(config['training']['out_dir'], 'test', 'worst'), 'all')
+    for i in range(batch_size):
+        save_imgs(out_embs[:, i, :config['data']['channels'], :, :], os.path.join(config['training']['out_dir'], 'test', 'worst'), '%d' % i, True)
+
+    print('Plotting Persistence...')
+    images, labels = get_inputs(np.random.choice(len(trainset), batch_size, False), device)
+
+    for n in range(1, 9):
+        images_dec, out_embs = forward_pass(images, None if n == 1 else out_embs[-1, ...])
+        save_imgs(images_dec, os.path.join(config['training']['out_dir'], 'test', 'persist'), '%d' % n)
+
+    print('Plotting Regeneration...')
+    images, labels = get_inputs(np.random.choice(len(trainset), batch_size, False), device)
+
+    images_occ = torch.zeros(batch_size, n_filter, image_size, image_size, device=device)
+    images_occ[:, :config['data']['channels'], ...] = images.clone()
+    images_occ[:, :, image_size // 2:, :] = 0.0
+    images_dec, out_embs = forward_pass(images, images_occ)
+    save_imgs(images_occ[:, :config['data']['channels'], ...], os.path.join(config['training']['out_dir'], 'test', 'regen'), '0_occ')
+    save_imgs(images_dec, os.path.join(config['training']['out_dir'], 'test', 'regen'), '0')
+
+    images_occ = torch.zeros(batch_size, n_filter, image_size, image_size, device=device)
+    images_occ[:, :config['data']['channels'], ...] = images.clone()
+    images_occ[:, :, :image_size // 2, :] = 0.0
+    images_dec, out_embs = forward_pass(images, images_occ)
+    save_imgs(images_occ[:, :config['data']['channels'], ...], os.path.join(config['training']['out_dir'], 'test', 'regen'), '1_occ')
+    save_imgs(images_dec, os.path.join(config['training']['out_dir'], 'test', 'regen'), '1')
+
+    images_occ = torch.zeros(batch_size, n_filter, image_size, image_size, device=device)
+    images_occ[:, :config['data']['channels'], ...] = images.clone()
+    images_occ[:, :, :, image_size // 2:] = 0.0
+    images_dec, out_embs = forward_pass(images, images_occ)
+    save_imgs(images_occ[:, :config['data']['channels'], ...], os.path.join(config['training']['out_dir'], 'test', 'regen'), '2_occ')
+    save_imgs(images_dec, os.path.join(config['training']['out_dir'], 'test', 'regen'), '2')
+
+    images_occ = torch.zeros(batch_size, n_filter, image_size, image_size, device=device)
+    images_occ[:, :config['data']['channels'], ...] = images.clone()
+    images_occ[:, :, :, :image_size // 2] = 0.0
+    images_dec, out_embs = forward_pass(images, images_occ)
+    save_imgs(images_occ[:, :config['data']['channels'], ...], os.path.join(config['training']['out_dir'], 'test', 'regen'), '3_occ')
+    save_imgs(images_dec, os.path.join(config['training']['out_dir'], 'test', 'regen'), '3')
+
+    images_occ = torch.zeros(batch_size, n_filter, image_size, image_size, device=device)
+    images_occ[:, :config['data']['channels'], ...] = images.clone()
+    images_occ[:, :, image_size // 4:(image_size // 4) * 3, image_size // 4:(image_size // 4) * 3] = 0.0
+    images_dec, out_embs = forward_pass(images, images_occ)
+    save_imgs(images_occ[:, :config['data']['channels'], ...], os.path.join(config['training']['out_dir'], 'test', 'regen'), '4_occ')
+    save_imgs(images_dec, os.path.join(config['training']['out_dir'], 'test', 'regen'), '4')
+
