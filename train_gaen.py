7ff9ff4aac167c5d6235dc4f9337e464b05af99d
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2020-05-27 13:09:43 +0200

    added batch_mult to config

--- train_gaen.py
+++ train_gaen.py
@@ -91,7 +91,7 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
         running_loss_dis = np.zeros(window_size)
         running_loss_gen = np.zeros(window_size)
 
-        batch_mult = (int((epoch / config['training']['n_epochs']) * 4) + 1) * 4
+        batch_mult = (int((epoch / config['training']['n_epochs']) * config['training']['batch_mult_steps']) + 1) * config['training']['batch_mult']
 
         it = (epoch * len(trainloader))
 
