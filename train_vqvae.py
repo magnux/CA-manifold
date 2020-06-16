166f8e936e90c65d13f3cb1012120a7b0f6db01c
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2020-06-16 18:42:08 +0200

    making vae var again

--- train_vqvae.py
+++ train_vqvae.py
@@ -197,8 +197,8 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
             model_manager.log_manager.add_imgs(images_dec, 'all_dec', it)
             for lab in range(config['training']['sample_labels']):
                 fixed_lab = torch.full((batch_size,), lab, device=device, dtype=torch.int64)
-                lat_gen, lat_labs = generator(z_test, fixed_lab)
-                images_gen, _, _ = decoder(lat_gen, inj_lat=lat_labs)
+                lat_gen = var_decoder(z_test, fixed_lab)
+                images_gen, _, _ = decoder(lat_gen)
                 model_manager.log_manager.add_imgs(images_gen, 'class_%04d' % lab, it)
 
         # Perform inception
