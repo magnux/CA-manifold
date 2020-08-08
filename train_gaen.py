2303fb46a248ba2ccc0ec8dedead445665dd11c7
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2020-08-08 14:25:52 +0200

    new greg

--- train_gaen.py
+++ train_gaen.py
@@ -209,7 +209,7 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
 
                         if g_reg_every > 0 and it % g_reg_every == 0:
                             if alt_reg:
-                                reg_gen_enc = (1 / batch_mult) * g_reg_every * d_reg_param * compute_grad_reg(lat_enc, images)
+                                reg_gen_enc = (1 / batch_mult) * g_reg_every * compute_grad_reg(lat_enc, images).sqrt()
                                 model_manager.loss_backward(reg_gen_enc, nets_to_train, retain_graph=True)
                                 reg_gen_enc_sum += reg_gen_enc.item()
                             else:
@@ -231,7 +231,7 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
 
                         if g_reg_every > 0 and it % g_reg_every == 0:
                             if alt_reg:
-                                reg_gen_dec = (1 / batch_mult) * g_reg_every * d_reg_param * compute_grad_reg(images_dec, lat_gen)
+                                reg_gen_dec = (1 / batch_mult) * g_reg_every * compute_grad_reg(images_dec, lat_gen).sqrt()
                                 model_manager.loss_backward(reg_gen_dec, nets_to_train, retain_graph=True)
                                 reg_gen_dec_sum += reg_gen_dec.item()
                             else:
