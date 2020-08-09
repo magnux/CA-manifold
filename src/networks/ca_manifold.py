098297d012d8dd828a91020f8819eb8321cafcce
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2020-08-09 18:28:02 +0200

    added switches for ablation study

--- src/networks/ca_manifold.py
+++ src/networks/ca_manifold.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 import torch.utils.data
 import torch.utils.data.distributed
 from src.layers.sobel import Sobel
@@ -18,15 +19,34 @@ class Decoder(nn.Module):
         self.n_filter = n_filter
         self.lat_size = lat_size
         self.n_calls = n_calls * 16
-        self.leak_factor = nn.Parameter(torch.ones([]) * 0.1)
         self.perception_noise = perception_noise
         self.fire_rate = fire_rate
+        self.fixed_conv = kwargs['fixed_conv'] if 'fixed_conv' in kwargs else False
+        self.alive_masking = kwargs['alive_masking'] if 'alive_masking' in kwargs else False
+        self.deactivate_norm = kwargs['deactivate_norm'] if 'deactivate_norm' in kwargs else False
+        self.fix_leak_1 = kwargs['fix_leak_1'] if 'fix_leak_1' in kwargs else False
+
+        if self.fix_leak_1:
+            self.leak_factor = 1.
+        else:
+            self.leak_factor = nn.Parameter(torch.ones([]) * 0.1)
 
         self.frac_sobel = Sobel(self.n_filter)
-        self.frac_norm = nn.InstanceNorm2d(self.n_filter * 3)
-        self.frac_dyna_conv = DynaConvBlock(self.lat_size, self.n_filter * 3, self.n_filter)
+
+        if not self.deactivate_norm:
+            self.frac_norm = nn.InstanceNorm2d(self.n_filter * 3)
+
+        if self.fixed_conv:
+            self.frac_conv = nn.Sequential(
+                nn.Conv2d(self.n_filter, self.n_filter * 8, 1, 1, 0),
+                nn.ReLU(True),
+                nn.Conv2d(self.n_filter * 8, self.n_filter, 1, 1, 0),
+            )
+        else:
+            self.frac_dyna_conv = DynaConvBlock(self.lat_size, self.n_filter * 3, self.n_filter)
 
     def forward(self, lat, ca_init=None):
+        assert self.fixed_conv == lat is None, 'when using fixed convs, the model is fixed to produce a single output, thus the latent should be None'
         batch_size = lat.size(0)
         float_type = torch.float16 if isinstance(lat, torch.cuda.HalfTensor) else torch.float32
 
@@ -43,13 +63,23 @@ class Decoder(nn.Module):
         leak_factor = torch.clamp(self.leak_factor, 1e-3, 1e3)
         for c in range(self.n_calls):
             out_new = out
+            if self.alive_masking:
+                pre_life_mask = F.max_pool2d(out_new[:, 3:4, :, :], 3, 1, 1) > 0.1
             if self.perception_noise and self.training:
                 out_new = out_new + (noise_mask[:, c].view(batch_size, 1, 1, 1) * torch.randn_like(out_new))
             out_new = self.frac_sobel(out_new)
-            out_new = self.frac_norm(out_new)
-            out_new = self.frac_dyna_conv(out_new, lat)
+            if not self.dectivate_norm:
+                out_new = self.frac_norm(out_new)
+            if self.fixed_conv:
+                out_new = self.frac_conv(out_new)
+            else:
+                out_new = self.frac_dyna_conv(out_new, lat)
             if self.fire_rate < 1.0:
                 out_new = out_new * (torch.rand([batch_size, 1, self.image_size, self.image_size], device=lat.device) <= self.fire_rate).to(float_type)
+            if self.alive_masking:
+                post_life_mask = F.max_pool2d(out_new[:, 3:4, :, :], 3, 1, 1) > 0.1
+                life_mask = (pre_life_mask & post_life_mask).to(float_type)
+                out = out_new * life_mask
             out = out + (leak_factor * out_new)
             out_embs.append(out)
 
