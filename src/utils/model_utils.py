e8720637c6498607ab4444d16077d4ad0ca95944
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2020-08-19 17:38:40 +0200

    trying different things

--- src/utils/model_utils.py
+++ src/utils/model_utils.py
@@ -199,7 +199,7 @@ class KalmanFilter:
         self.P[it] = (1 - self.K[it]) * self.Pminus[it]
 
         if it > 0:
-            self.R += np.var(self.z[max(0, it - int(1e-3 * self.total_it)):it]) - self.R
+            self.R = 0.9 * self.R + 0.1 * np.var(self.z[max(0, it - int(1e-3 * self.total_it)):it])
             self.Q = 1e-3 * self.R
 
         self.last_it = it
