5fd1886ed19dd75390e33325b1520be86755c900
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2020-08-20 16:09:07 +0200

    trying different things

--- train_gaen.py
+++ train_gaen.py
@@ -203,7 +203,7 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                             model_manager.loss_backward(reg_dis_dec, nets_to_train, retain_graph=True)
                             reg_dis_dec_sum += reg_dis_dec.item()
 
-                        if d_reg_every == 0 or d_reg_every >= 1. or it % int(1 / d_reg_every) == 0:
+                        if d_reg_every == 0 or d_reg_every >= 1 or it % int(1 / d_reg_every) == 0:
                             model_manager.loss_backward(loss_dis_dec, nets_to_train)
                         loss_dis_dec_sum += loss_dis_dec.item()
 
@@ -212,7 +212,7 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                         reg_dis_dec_mean = reg_dis_dec_est.update_kf(it, reg_dis_dec_sum / max(1, d_reg_every))
                         max_reg = max(reg_dis_enc_mean, reg_dis_dec_mean)
                         d_reg_every_est = np.clip(d_reg_every_est + (reg_dis_target - max_reg), 1 / config['training']['d_reg_every'], config['training']['d_reg_every'])
-                        d_reg_every = int(d_reg_every_est)
+                        d_reg_every = int(d_reg_every_est) if d_reg_every >= 1 else d_reg_every_est
 
                 # Generator step
                 with model_manager.on_step(['encoder', 'decoder', 'generator']) as nets_to_train:
