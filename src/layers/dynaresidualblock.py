9a30d90c1e9638706eada86f69659d45f3c46674
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2020-09-04 18:17:30 +0200

    trying new transformer approach

--- src/layers/dynaresidualblock.py
+++ src/layers/dynaresidualblock.py
@@ -41,11 +41,11 @@ class DynaResidualBlock(nn.Module):
         k_total_size = (self.k_in_size + self.k_mid_size + self.k_out_size + self.k_short_size +
                         self.b_in_size + self.b_mid_size + self.b_out_size + self.b_short_size)
 
-        n_blocks = 3
+        n_blocks = 4
         self.dyna_k = nn.Sequential(
             nn.Linear(self.lat_size, ceil(k_total_size / self.fhidden) * self.fhidden),
             LambdaLayer(lambda x: x.view(x.size(0), self.fhidden, ceil(k_total_size / self.fhidden))),
-            * list(chain(*[[ResidualAttentionBlock(ceil(k_total_size / self.fhidden), self.fhidden, self.fhidden // 2, 4),
+            * list(chain(*[[ResidualAttentionBlock(ceil(k_total_size / self.fhidden), self.fhidden, self.fhidden // 4),
                             ResidualBlock(self.fhidden, self.fhidden, self.fhidden // 2, 1, 1, 0, nn.Conv1d)] for _ in range(n_blocks)])),
             LambdaLayer(lambda x: x.view(x.size(0), ceil(k_total_size / self.fhidden) * self.fhidden)),
             LambdaLayer(lambda x: x[:, :k_total_size]),
--- src/networks/ca_manifold_2.py
+++ src/networks/ca_manifold_2.py
@@ -58,14 +58,7 @@ class InjectedEncoder(nn.Module):
 
         self.out_norm = nn.InstanceNorm2d(self.n_filter)
         self.out_conv = ResidualBlock(self.n_filter, sum(self.split_sizes), None, 1, 1, 0)
-        n_blocks = 3
-        self.out_to_lat = nn.Sequential(
-            LambdaLayer(lambda x: x.view(x.size(0), self.n_filter, sum(self.conv_state_size) // self.n_filter)),
-            *list(chain(*[[ResidualAttentionBlock(sum(self.conv_state_size) // self.n_filter, self.n_filter, self.n_filter // 2, 4),
-                           ResidualBlock(self.n_filter, self.n_filter, self.n_filter // 2, 1, 1, 0, nn.Conv1d)] for _ in range(n_blocks)])),
-            LambdaLayer(lambda x: x.view(x.size(0), sum(self.conv_state_size))),
-            nn.Linear(sum(self.conv_state_size), self.lat_size),
-        )
+        self.out_to_lat = nn.Linear(sum(self.conv_state_size), self.lat_size)
 
     def forward(self, x, inj_lat=None):
         assert (inj_lat is not None) == self.injected, 'latent should only be passed to injected encoders'
