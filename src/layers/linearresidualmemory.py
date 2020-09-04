9397dcf21f29a492beb2cf0aeba7e111743062af
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2020-09-04 20:30:16 +0200

    trying yet another approach

--- src/layers/linearresidualmemory.py
+++ src/layers/linearresidualmemory.py
@@ -1,5 +1,6 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
 
 class LinearResidualMemory(nn.Module):
@@ -11,17 +12,20 @@ class LinearResidualMemory(nn.Module):
 
         self.q = nn.Linear(fin, fin * n_mem)
         self.k = nn.Linear(fin, fin * n_mem)
-        self.v = nn.Linear(fin, fin * n_mem)
+        self.v = nn.Linear(fin, (fin + 1) * n_mem)
+        self.l_out = nn.Linear((fin + 1), fin)
 
     def forward(self, x):
         batch_size = x.size(0)
 
         x_q = self.q(x).view(batch_size,  self.n_mem, self.fin)
         x_k = self.k(x).view(batch_size,  self.n_mem, self.fin).permute(0, 2, 1)
-        x_v = self.v(x).view(batch_size,  self.n_mem, self.fin)
+        x_v = self.v(x).view(batch_size,  self.n_mem, self.fin + 1)
 
         mem_x = torch.bmm(x_q, x_k)
         mem_x = torch.bmm(mem_x, x_v)
-        mem_x = mem_x.mean(1)
+        mem_x = mem_x.sum(1)
+        mem_x = F.normalize(mem_x)
+        mem_x = self.l_out(mem_x)
 
         return x + mem_x
