663eb5f78a87b9fb6ba436641253a3a3a8064f31
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2020-09-14 14:19:57 +0200

    added z inv loss

--- train_gaen.py
+++ train_gaen.py
@@ -181,7 +181,8 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
 
                         with torch.no_grad():
                             z_reenc, _ = get_z(lat_enc, labels)
-                            lat_reenc = generator(-1 * z_reenc, labels)
+                            z_reenc = F.normalize(z_reenc + 0.1 * torch.randn_like(z_reenc))
+                            lat_reenc = generator(z_reenc, labels)
                             images_dec, _, _ = decoder(lat_reenc)
 
                         lat_reenc.requires_grad_()
@@ -242,7 +243,8 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                             z_reenc, _ = get_z(lat_enc, labels)
 
                         z_reenc.requires_grad_()
-                        lat_reenc = generator(-1 * z_reenc, labels)
+                        z_reenc = F.normalize(z_reenc + 0.1 * torch.randn_like(z_reenc))
+                        lat_reenc = generator(z_reenc, labels)
                         images_dec, _, _ = decoder(lat_reenc)
                         lat_top_dec, _, _ = dis_encoder(images_dec, lat_reenc)
                         labs_dec = discriminator(lat_top_dec, labels)
