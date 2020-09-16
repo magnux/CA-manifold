4be7fc77085119c3c351b9733e71e8774a4e4bc9
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2020-09-16 09:20:07 +0200

    trying with scale and shift

--- train_gaen.py
+++ train_gaen.py
@@ -222,7 +222,7 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
 
                         lat_gen = generator(z_gen, labels)
                         images_dec, _, _ = decoder(lat_gen)
-                        lat_top_dec, _, _ = dis_encoder(images_dec, lat_gen)
+                        lat_top_dec, _, _ = dis_encoder(images_dec, lat_gen.detach())
                         labs_dec = discriminator(lat_top_dec, labels)
 
                         if g_reg_every > 0 and it % g_reg_every == 0:
