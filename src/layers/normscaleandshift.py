85eccfa653acb6fce6c6ffc0e1ad142f367ed9da
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2020-09-16 13:44:50 +0200

    trying with scale and shift

--- src/layers/normscaleandshift.py
+++ src/layers/normscaleandshift.py
@@ -11,17 +11,10 @@ class NormScaleAndShift(nn.Module):
 
     def forward(self, x):
         s = self.s if self.s is not None else x.new_zeros(x.size())
-        # s = s.clamp(-1e-3, 1.)  # Safety first
         t = self.t if self.t is not None else x.new_zeros(x.size())
+        s = s.clamp(-1e-3, 1e3)  # Safety first
         z = x * torch.exp(s) + t
         return z
-
-    def backward(self, z):
-        s = self.s if self.s is not None else z.new_zeros(z.size())
-        # s = s.clamp(-1e-3, 1.)  # Safety first
-        t = self.t if self.t is not None else z.new_zeros(z.size())
-        x = (z - t) * torch.exp(-s)
-        return x
 
 
 class DeNormScaleAndShift(nn.Module):
@@ -33,14 +26,7 @@ class DeNormScaleAndShift(nn.Module):
 
     def forward(self, z):
         s = self.s if self.s is not None else z.new_zeros(z.size())
-        # s = s.clamp(-1e-3, 1.)  # Safety first
         t = self.t if self.t is not None else z.new_zeros(z.size())
+        s = s.clamp(-1e-3, 1e3)  # Safety first
         x = (z - t) * torch.exp(-s)
         return x
-
-    def backward(self, x):
-        s = self.s if self.s is not None else x.new_zeros(x.size())
-        # s = s.clamp(-1e-3, 1.)  # Safety first
-        t = self.t if self.t is not None else x.new_zeros(x.size())
-        z = x * torch.exp(s) + t
-        return z
