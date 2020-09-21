00efdf61ae690f217a28f2d98fe2fd7062198e27
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2020-09-21 09:24:38 +0200

    small fixes

--- src/utils/loss_utils.py
+++ src/utils/loss_utils.py
@@ -57,26 +57,24 @@ def compute_grad_reg(d_out, d_in, norm_type=2, margin=0):
 
 def update_reg_params(reg_every, reg_every_target, reg_param, reg_loss, reg_loss_target,
                       reg_elapsed, loss_dis, est_type='squared', update_every=True):
-    # reg_every update
-    if update_every:
-        if reg_loss / reg_loss_target < 1:
-            reg_every += 1
-        else:
-            reg_every -= 1
-        reg_every = np.clip(reg_every, 1, reg_every_target)
-
     # reg_param update
     delta_reg = reg_loss_target - reg_loss
     if est_type == 'linear':
         # Linear update estimate
-        delta_reg = delta_reg * reg_elapsed
+        reg_param -= delta_reg * reg_elapsed
     elif est_type == 'squared':
         # Squared update estimate
-        reg_scale = reg_loss_target / 10
-        delta_reg = np.sign(delta_reg) * ((delta_reg / reg_scale) ** 2) * reg_scale * reg_elapsed
-
-    reg_param -= delta_reg
+        reg_scale = reg_loss_target / 2
+        reg_param -= np.sign(delta_reg) * ((delta_reg / reg_scale) ** 2) * reg_scale * reg_elapsed
     reg_param = np.clip(reg_param, 1e-9, 1e9)
+
+    # reg_every update
+    if update_every:
+        if delta_reg / reg_loss_target < 0.1:
+            reg_every += 1
+        else:
+            reg_every -= 1
+        reg_every = np.clip(reg_every, 1, reg_every_target)
 
     # Emergency break, in case the discriminator had slowly slip through the fence
     if loss_dis < 1e-2:
