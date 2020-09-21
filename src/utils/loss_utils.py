6e0f8e27fadc9449798e3ca820adb942d5383568
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2020-09-21 09:35:03 +0200

    small fixes

--- src/utils/loss_utils.py
+++ src/utils/loss_utils.py
@@ -64,13 +64,12 @@ def update_reg_params(reg_every, reg_every_target, reg_param, reg_loss, reg_loss
         reg_param -= delta_reg * reg_elapsed
     elif est_type == 'squared':
         # Squared update estimate
-        reg_scale = reg_loss_target * 10
-        reg_param -= np.sign(delta_reg) * ((delta_reg / reg_scale) ** 2) * reg_scale * reg_elapsed
+        reg_param -= np.sign(delta_reg) * ((delta_reg / reg_loss_target) ** 2) * reg_loss_target * reg_elapsed
     reg_param = np.clip(reg_param, 1e-9, 1e9)
 
     # reg_every update
     if update_every:
-        if delta_reg / reg_loss_target < 0.1:
+        if np.abs(delta_reg / reg_loss_target) < 0.1:
             reg_every += 1
         else:
             reg_every -= 1
