82566c1864f2f1894d3969ab51bc127450b0f28a
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2020-09-22 15:19:14 +0200

    trying with pl reg

--- train_gaen.py
+++ train_gaen.py
@@ -223,10 +223,10 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                         labs_enc = discriminator(lat_top_enc, labels)
 
                         if g_reg_every > 0 and it % g_reg_every == 0:
-                            reg_gen_enc, pl_mean_enc = compute_pl_reg(lat_enc, images, pl_mean_enc, min_pl=0.1 * reg_dis_target)
-                            reg_gen_enc = (1 / batch_mult) * g_reg_ratio * g_reg_every * reg_gen_enc
+                            reg_gen_enc, pl_mean_enc = compute_pl_reg(lat_enc, images, pl_mean_enc, min_pl=max(0.1 * reg_dis_target, pl_mean_dec))
+                            reg_gen_enc = (1 / batch_mult) * g_reg_every * reg_gen_enc
                             model_manager.loss_backward(reg_gen_enc, nets_to_train, retain_graph=True)
-                            reg_gen_enc_sum += reg_gen_enc.item() / (g_reg_ratio * g_reg_every)
+                            reg_gen_enc_sum += reg_gen_enc.item() / g_reg_every
 
                         loss_gen_enc = (1 / batch_mult) * compute_gan_loss(labs_enc, 0)
                         model_manager.loss_backward(loss_gen_enc, nets_to_train)
@@ -241,10 +241,10 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                             # if alt_reg:
                             #     reg_gen_dec, pl_mean_dec = compute_pl_reg_dct(images_dec, lat_gen, pl_mean_dec)
                             # else:
-                            reg_gen_dec, pl_mean_dec = compute_pl_reg(images_dec, lat_gen, pl_mean_dec, min_pl=0.1 * reg_dis_target)
-                            reg_gen_dec = (1 / batch_mult) * (1 / g_reg_ratio) * g_reg_every * reg_gen_dec
+                            reg_gen_dec, pl_mean_dec = compute_pl_reg(images_dec, lat_gen, pl_mean_dec, min_pl=max(0.1 * reg_dis_target, pl_mean_enc))
+                            reg_gen_dec = (1 / batch_mult) * g_reg_every * reg_gen_dec
                             model_manager.loss_backward(reg_gen_dec, nets_to_train, retain_graph=True)
-                            reg_gen_dec_sum += reg_gen_dec.item() / ((1 / g_reg_ratio) * g_reg_every)
+                            reg_gen_dec_sum += reg_gen_dec.item() / g_reg_every
 
                         loss_gen_dec = (1 / batch_mult) * compute_gan_loss(labs_dec, 1)
                         model_manager.loss_backward(loss_gen_dec, nets_to_train)
