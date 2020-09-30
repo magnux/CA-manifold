387dc140d6dea1405a1de6aa3f31e05f021acf5b
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2020-09-30 18:06:01 +0200

    Trying without ratio

--- src/networks/base.py
+++ src/networks/base.py
@@ -49,8 +49,7 @@ class Generator(nn.Module):
         self.register_buffer('embedding_mat', torch.eye(n_labels))
         self.embedding_fc = nn.Linear(n_labels, embed_size, bias=False)
         self.embed_to_lat = nn.Linear(z_dim + embed_size, self.lat_size, bias=False)
-        nn.init.normal_(self.embed_to_lat.weight, (1 / ((z_dim + embed_size) ** 0.5)))  # *  # Recovering std == 1
-        # ((channels * image_size * image_size) ** 0.5) / (self.lat_size ** 0.5))  # Renormalizing to expected norm
+        nn.init.normal_(self.embed_to_lat.weight, (1 / (self.lat_size * (z_dim + embed_size) ** 0.5)))  # *  # std == 1 / lat_size
 
     def forward(self, z, y):
         assert (z.size(0) == y.size(0))
@@ -60,7 +59,7 @@ class Generator(nn.Module):
         else:
             yembed = y
 
-        z = F.normalize(z)
+        # z = F.normalize(z)
         yembed = self.embedding_fc(yembed)
         yembed = F.normalize(yembed)
         lat = self.embed_to_lat(torch.cat([z, yembed], dim=1))
@@ -75,7 +74,7 @@ class LabsEncoder(nn.Module):
         self.register_buffer('embedding_mat', torch.eye(n_labels))
         self.embedding_fc = nn.Linear(n_labels, embed_size, bias=False)
         self.embed_to_lat = nn.Linear(embed_size, self.lat_size, bias=False)
-        nn.init.normal_(self.embed_to_lat.weight, (1 / (embed_size ** 0.5)))
+        nn.init.normal_(self.embed_to_lat.weight, (1 / (self.lat_size * embed_size ** 0.5)))
 
     def forward(self, y):
         if y.dtype is torch.int64:
@@ -107,10 +106,10 @@ class UnconditionalGenerator(nn.Module):
         super().__init__()
         self.lat_size = lat_size
         self.embed_to_lat = nn.Linear(z_dim, self.lat_size, bias=False)
-        nn.init.normal_(self.embed_to_lat.weight, (1 / (z_dim ** 0.5)))
+        nn.init.normal_(self.embed_to_lat.weight, (1 / (self.lat_size * z_dim ** 0.5)))
 
     def forward(self, z):
-        z = F.normalize(z)
+        # z = F.normalize(z)
         lat = self.embed_to_lat(z)
 
         return lat
