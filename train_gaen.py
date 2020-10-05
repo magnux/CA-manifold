43d2fe134834f4ecf09650ffdf572ef80757cccd
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2020-10-05 12:57:02 +0200

    several small changes and model simplification

--- train_gaen.py
+++ train_gaen.py
@@ -129,7 +129,7 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
         running_loss_gen = np.zeros(window_size)
 
         batch_mult = (int((epoch / config['training']['n_epochs']) * config['training']['batch_mult_steps']) + 1) * batch_split
-        reg_dis_target = 1e-2 * ((1 + 1e-2) - (epoch / config['training']['n_epochs']))
+        reg_dis_target = 1e-6 * ((1 + 1e-6) - (epoch / config['training']['n_epochs']))
         it = epoch * (len(trainloader) // batch_split)
 
         t = trange(len(trainloader) // batch_split)
