5bea6298067c4b7949d946d4d2d5235ef92f575b
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2020-10-31 17:54:35 +0100

    added gage model

--- train_gage.py
+++ train_gage.py
@@ -131,13 +131,13 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                         model_manager.loss_backward(loss_dis_enc, nets_to_train, retain_graph=True)
                         loss_dis_enc_sum += loss_dis_enc.item()
 
-                        # lat_enc = generator(z_enc, labels)
-                        # _, out_embs, _ = decoder(lat_enc)
-                        # images_redec, _, _ = decoder(lat_enc, out_embs[-1])
-                        #
-                        # loss_enc = (1 / batch_mult) * F.mse_loss(images_redec, images)
-                        # model_manager.loss_backward(loss_enc, nets_to_train)
-                        # loss_enc_sum += loss_enc.item()
+                        lat_enc = generator(z_enc, labels)
+                        _, out_embs, _ = decoder(lat_enc)
+                        images_redec, _, _ = decoder(lat_enc, out_embs[-1])
+
+                        loss_enc = (1 / batch_mult) * F.mse_loss(images_redec, images)
+                        model_manager.loss_backward(loss_enc, nets_to_train)
+                        loss_enc_sum += loss_enc.item()
 
                         with torch.no_grad():
                             lat_gen = generator(z_gen, labels)
@@ -147,7 +147,7 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                         z_dec, _, _ = encoder(images_dec, labels)
 
                         loss_dis_dec = (1 / batch_mult) * -age_gaussian_kl_loss(F.normalize(z_dec, dim=1))
-                        model_manager.loss_backward(loss_dis_dec, nets_to_train, retain_graph=True)
+                        model_manager.loss_backward(loss_dis_dec, nets_to_train)
                         loss_dis_dec_sum += loss_dis_dec.item()
 
                         z_redec, _, _ = encoder(images_redec, labels)
