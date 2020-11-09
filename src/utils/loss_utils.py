5dd565b9d61d730da910781f3bf5f1e2e5ecce21
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2020-11-09 09:18:43 +0100

    readded dynamic lazy reg

--- src/utils/loss_utils.py
+++ src/utils/loss_utils.py
@@ -65,7 +65,7 @@ def compute_grad_reg(d_out, d_in, norm_type=2, margin=0):
 
 
 def update_reg_params(reg_every, reg_every_target, reg_param, reg_param_target, reg_loss, reg_loss_target,
-                      loss_dis=None, update_every=False, maximize=True):
+                      loss_dis=None, update_every=True, maximize=True):
     lr = 1e-2 * reg_param_target
 
     # reg_param update
@@ -80,11 +80,11 @@ def update_reg_params(reg_every, reg_every_target, reg_param, reg_param_target, 
 
     # reg_every update
     if update_every:
-        if np.abs(reg_update / reg_param) < 0.01:
-            reg_every = reg_every_target
+        if np.abs(reg_update / reg_param) < 0.1:
+            reg_every += 1
         else:
-            reg_every = 1
-        # reg_every = np.clip(reg_every, 1, reg_every_target)
+            reg_every -= 1
+        reg_every = np.clip(reg_every, 1, reg_every_target)
 
     if loss_dis is not None:
         # Emergency break, in case the discriminator had slowly slip through the fence
