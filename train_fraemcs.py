2a6553640795f1d01e1ea41ec6c5bafb8e91895f
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-01-12 21:04:41 +0100

    testing new fraemcs model

--- train_fraemcs.py
+++ train_fraemcs.py
@@ -141,7 +141,7 @@ if pre_train:
 
                             z_enc, _, _ = encoder(images, labels)
                             lat_enc = generator(z_enc)
-                            images_dec, _, _ = decoder(lat_enc, seed=seed_idx[torch.ones_like(labels) * n_labels + 1, it % n_seed].tolist())
+                            images_dec, _, _ = decoder(lat_enc, seed=seed_idx[torch.ones_like(labels) * n_labels, it % n_seed].tolist())
 
                             loss_dec = (1 / batch_split) * F.mse_loss(images_dec, images)
                             model_manager.loss_backward(loss_dec, nets_to_train)
@@ -151,7 +151,7 @@ if pre_train:
                 with torch.no_grad():
                     z_enc, _, _ = encoder(images_test, labels_test)
                     lat_enc = generator(z_enc)
-                    images_dec, _, _ = decoder(lat_enc, seed=seed_idx[torch.ones_like(labels) * n_labels + 1, :].tolist())
+                    images_dec, _, _ = decoder(lat_enc, seed=seed_idx[torch.ones_like(labels) * n_labels, :].tolist())
 
                 stream_images(images_dec, config_name + '/fraemcs_pretrain', config['training']['out_dir'] + '/fraemcs_pretrain')
 
@@ -276,7 +276,7 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                             images[batch_split_size // 2:, ...].data.copy_(images_redec_half)
                         z_enc, _, _ = encoder(images, labels)
                         lat_enc = generator(z_enc)
-                        images_dec, _, _ = decoder(lat_enc, seed_n=seed_idx[torch.ones_like(labels) * n_labels + 1, it % n_seed].tolist())
+                        images_dec, _, _ = decoder(lat_enc, seed_n=seed_idx[torch.ones_like(labels) * n_labels, it % n_seed].tolist())
 
                         loss_dec = (1 / batch_mult) * F.mse_loss(images_dec, images)
                         model_manager.loss_backward(loss_dec, nets_to_train, retain_graph=True)
@@ -304,7 +304,7 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                 # Streaming Images
                 with torch.no_grad():
                     lat_gen = generator(z_test)
-                    images_gen, _, _ = decoder(lat_gen, seed_n=seed_idx[torch.ones_like(labels) * n_labels + 1, :].tolist())
+                    images_gen, _, _ = decoder(lat_gen, seed_n=seed_idx[torch.ones_like(labels) * n_labels, :].tolist())
                     images_regen, _, _ = decoder(lat_gen, seed_n=seed_idx[labels_test, :].tolist())
                     images_gen = torch.cat([images_gen, images_regen], dim=3)
 
@@ -337,12 +337,12 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
             t.write('Creating samples...')
             images, labels, z_gen, trainiter = get_inputs(trainiter, batch_size, device)
             lat_gen = generator(z_test)
-            images_gen, _, _ = decoder(lat_gen, seed_n=seed_idx[torch.ones_like(labels) * n_labels + 1, :].tolist())
+            images_gen, _, _ = decoder(lat_gen, seed_n=seed_idx[torch.ones_like(labels) * n_labels, :].tolist())
             images_regen, _, _ = decoder(lat_gen, seed_n=seed_idx[labels_test, :].tolist())
             images_gen = torch.cat([images_gen, images_regen], dim=3)
             z_enc, _, _ = encoder(images, labels)
             lat_enc = generator(z_enc)
-            images_dec, _, _ = decoder(lat_enc, seed_n=seed_idx[torch.ones_like(labels) * n_labels + 1, :].tolist())
+            images_dec, _, _ = decoder(lat_enc, seed_n=seed_idx[torch.ones_like(labels) * n_labels, :].tolist())
             images_redec, _, _ = decoder(lat_enc, seed_n=seed_idx[labels, :].tolist())
             images_dec = torch.cat([images_dec, images_redec], dim=3)
             model_manager.log_manager.add_imgs(images, 'all_input', it)
@@ -355,7 +355,7 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                     fixed_lab = labels_test.clone()
                     fixed_lab[:, lab] = 1
                 lat_gen = generator(z_test)
-                images_gen, _, _ = decoder(lat_gen, seed_n=seed_idx[torch.ones_like(labels) * n_labels + 1, :].tolist())
+                images_gen, _, _ = decoder(lat_gen, seed_n=seed_idx[torch.ones_like(labels) * n_labels, :].tolist())
                 images_regen, _, _ = decoder(lat_gen, seed_n=seed_idx[labels, :].tolist())
                 images_gen = torch.cat([images_gen, images_regen], dim=3)
                 model_manager.log_manager.add_imgs(images_gen, 'class_%04d' % lab, it)
