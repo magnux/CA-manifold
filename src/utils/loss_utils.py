aaa277837b6b534599b30e56cc3250319cb3c5c6
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-01-18 15:16:01 +0100

    made more strict update reg

--- src/utils/loss_utils.py
+++ src/utils/loss_utils.py
@@ -66,7 +66,7 @@ def compute_grad_reg(d_out, d_in, norm_type=2, margin=0):
 
 def update_reg_params(reg_every, reg_every_target, reg_param, reg_param_target, reg_loss, reg_loss_target,
                       loss_dis=None, update_every=True, maximize=True):
-    lr = 0.1
+    lr = 1.
 
     # reg_param update
     delta_reg = reg_loss_target - reg_loss
