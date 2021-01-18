aa4a0146d0185922667ceba679c95fdc6ecd2160
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-01-18 18:25:40 +0100

    testing with deactivated reg_every

--- src/utils/loss_utils.py
+++ src/utils/loss_utils.py
@@ -65,7 +65,7 @@ def compute_grad_reg(d_out, d_in, norm_type=2, margin=0):
 
 
 def update_reg_params(reg_every, reg_every_target, reg_param, reg_param_target, reg_loss, reg_loss_target,
-                      loss_dis=None, update_every=False, maximize=True):
+                      loss_dis=None, update_every=True, maximize=True):
     lr = 0.1
 
     # reg_param update
@@ -82,20 +82,15 @@ def update_reg_params(reg_every, reg_every_target, reg_param, reg_param_target, 
         reg_ratio = (reg_loss / reg_loss_target)
         if reg_ratio <= 1.:
             reg_every += 1
-            reg_adjust = reg_param / reg_every
-            if maximize:
-                reg_param -= reg_adjust
-            else:
-                reg_param += reg_adjust
         elif reg_ratio > 2.:
             reg_every /= 2
 
     if loss_dis is not None:
         # Emergency break, in case the discriminator had slowly slip through the fence
-        if loss_dis < 1e-2:
+        if loss_dis < 0.1:
             if update_every:
                 reg_every = 1
-            reg_param /= 2
+            reg_param = 1e-18
 
     reg_param = np.clip(reg_param, 1e-18, 1e18)
     reg_every = np.clip(reg_every, 1, reg_every_target)
