e10032d0d7bf6e8f262436e3678cc5336812e4d5
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-02-22 20:48:01 +0100

    rollback on dynaconv, testing cubegen

--- src/networks/base.py
+++ src/networks/base.py
@@ -66,7 +66,7 @@ class Generator(nn.Module):
         self.register_buffer('embedding_mat', torch.eye(n_labels))
         self.n_filter = n_filter
         self.n_calls = n_calls
-        self.frac_sobel = SinSobel(self.n_filter, 3, 1, dim=3)
+        self.frac_sobel = SinSobel(self.n_filter, [3, 5], [1, 2], dim=3)
         self.frac_norm = nn.InstanceNorm2d(self.n_filter * self.frac_sobel.c_factor)
         self.frac_dyna_conv = DynaResidualBlock(n_labels, self.n_filter * self.frac_sobel.c_factor, self.n_filter, self.n_filter, dim=3)
 
