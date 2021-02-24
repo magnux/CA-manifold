060e6603ae80823079e36b8faf5b4bf12fc45015
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-02-24 19:00:31 +0100

    testing a different control eq

--- src/utils/loss_utils.py
+++ src/utils/loss_utils.py
@@ -65,14 +65,11 @@ def compute_grad_reg(d_out, d_in, norm_type=2, margin=0):
     return reg
 
 
-def update_reg_params(reg_every, reg_every_target, reg_param, reg_param_target, reg_loss, reg_loss_target,
-                      loss_dis=None, update_every=True, maximize=True):
-    lr = 0.1
-
+def update_reg_params(reg_every, reg_every_target, reg_param, reg_loss, reg_loss_target,
+                      loss_dis=None, update_every=True, maximize=True, lr=0.1):
     # reg_param update
     delta_reg = reg_loss_target - reg_loss
-    reg_scale = reg_param / max(reg_loss_target, reg_loss)
-    reg_update = lr * reg_scale * delta_reg
+    reg_update = lr * delta_reg
     if maximize:
         reg_param += reg_update
     else:
--- train_fraems.py
+++ train_fraems.py
@@ -254,7 +254,7 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                         reg_dis_mean = 0.5 * (reg_dis_enc_sum + reg_dis_dec_sum)
                         loss_dis_mean = 0.5 * (loss_dis_enc_sum + loss_dis_dec_sum)
                         d_reg_every_mean = d_reg_every_mean_next
-                        d_reg_every_mean_next, d_reg_param_mean = update_reg_params(d_reg_every_mean_next, d_reg_every, d_reg_param_mean, d_reg_param,
+                        d_reg_every_mean_next, d_reg_param_mean = update_reg_params(d_reg_every_mean_next, d_reg_every, d_reg_param_mean,
                                                                                     reg_dis_mean, reg_dis_target, loss_dis_mean)
 
                 with model_manager.on_step(['encoder', 'decoder', 'generator']) as nets_to_train:
