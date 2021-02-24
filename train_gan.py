9dcd0448251ce7e2c11a840980643c4abe08150c
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-02-24 18:01:10 +0100

    testing running mean for better control

--- train_gan.py
+++ train_gan.py
@@ -190,8 +190,8 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                         loss_dis_dec_sum += loss_dis_dec.item()
 
                     if d_reg_every_mean > 0 and it % d_reg_every_mean == 0:
-                        reg_dis_mean = 0.5 * reg_dis_mean + 0.25 * (reg_dis_enc_sum + reg_dis_dec_sum)
-                        loss_dis_mean = 0.5 * loss_dis_mean + 0.25 * (loss_dis_enc_sum + loss_dis_dec_sum)
+                        reg_dis_mean = 0.9 * reg_dis_mean + 0.05 * (reg_dis_enc_sum + reg_dis_dec_sum)
+                        loss_dis_mean = 0.9 * loss_dis_mean + 0.05 * (loss_dis_enc_sum + loss_dis_dec_sum)
                         d_reg_every_mean = d_reg_every_mean_next
                         d_reg_every_mean_next, d_reg_param_mean = update_reg_params(d_reg_every_mean_next, d_reg_every, d_reg_param_mean, d_reg_param,
                                                                                     reg_dis_mean, reg_dis_target, loss_dis_mean)
