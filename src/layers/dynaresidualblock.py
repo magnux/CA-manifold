8b291429fbb2f41289edfcd5fdbb82e91551852b
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-02-26 15:58:09 +0100

    testing hs

--- src/layers/dynaresidualblock.py
+++ src/layers/dynaresidualblock.py
@@ -42,7 +42,7 @@ class DynaResidualBlock(nn.Module):
 
         self.dyna_k = nn.Sequential(
             nn.Linear(lat_size, self.lat_size),
-            *(chain(*[(LinearResidualBlock(self.lat_size, self.lat_size), HardSwish(True)) for _ in range(4)])),
+            LinearResidualBlock(self.lat_size, self.lat_size),
             LinearResidualBlock(self.lat_size, k_total_size, self.lat_size * 2),
         )
 
--- src/networks/pyracell_ae.py
+++ src/networks/pyracell_ae.py
@@ -68,7 +68,7 @@ class InjectedEncoder(nn.Module):
         self.out_conv = nn.Conv2d(self.n_filter, sum(self.split_sizes), 1, 1, 0)
         self.out_to_lat = nn.Sequential(
             LinearResidualBlock(sum(self.conv_state_size), self.lat_size, self.lat_size * 2),
-            *(chain(*[(LinearResidualBlock(self.lat_size, self.lat_size), HardSwish(True)) for _ in range(4)])),
+            LinearResidualBlock(self.lat_size, self.lat_size),
             nn.Linear(self.lat_size, lat_size if not z_out else z_dim)
         )
 
