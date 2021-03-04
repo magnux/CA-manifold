5a55422020d3fe9dcec7758dbe16fef127efc285
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-03-04 10:11:54 +0100

    made a few changes to the rage model

--- train_rage.py
+++ train_rage.py
@@ -206,17 +206,6 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                         # model_manager.loss_backward(loss_dis_dec, nets_to_train)
                         # loss_dis_dec_sum -= loss_dis_dec.item()
 
-                        with torch.no_grad():
-                            lat_gen = generator(z_gen, labels)
-                            images_dec, _, _ = decoder(lat_gen)
-                            images_redec, _, _ = decoder(lat_gen, img_init=images_dec)
-
-                        z_dec, _, _ = encoder(images_redec, labels)
-
-                        loss_enc = (1 / batch_mult) * F.l1_loss(z_dec, z_gen)
-                        model_manager.loss_backward(loss_enc, nets_to_train)
-                        loss_enc_sum += loss_enc.item()
-
                 # Generator step
                 with model_manager.on_step(['decoder', 'generator']) as nets_to_train:
 
@@ -244,9 +233,22 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                             model_manager.loss_backward(loss_gen_dec, nets_to_train)
                             loss_gen_dec_sum += loss_gen_dec.item()
 
-                        with torch.no_grad():
-                            z_enc, _, _ = encoder(images, labels)
+                # AE step
+                with model_manager.on_step(['encoder', 'decoder', 'generator']) as nets_to_train:
 
+                    for _ in range(batch_mult):
+                        images, labels, z_gen, trainiter = get_inputs(trainiter, batch_split_size, device)
+
+                        lat_gen = generator(z_gen, labels)
+                        images_dec, _, _ = decoder(lat_gen)
+                        images_redec, _, _ = decoder(lat_gen, img_init=images_dec)
+                        z_dec, _, _ = encoder(images_redec, labels)
+
+                        loss_enc = (1 / batch_mult) * F.l1_loss(z_dec, z_gen)
+                        model_manager.loss_backward(loss_enc, nets_to_train)
+                        loss_enc_sum += loss_enc.item()
+
+                        z_enc, _, _ = encoder(images, labels)
                         lat_enc = generator(z_enc, labels)
                         images_dec, _, _ = decoder(lat_enc)
 
