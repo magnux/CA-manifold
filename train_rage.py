7248a6216efbbabdda15406e754094f364de28da
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-03-04 15:48:21 +0100

    made a few changes to the rage model

--- train_rage.py
+++ train_rage.py
@@ -209,24 +209,22 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                     for _ in range(batch_mult):
                         images, labels, z_gen, trainiter = get_inputs(trainiter, batch_split_size, device)
 
-                        if (it % 2) == 0:
-                            lat_gen = generator(z_gen, labels)
-                            images_dec, _, _ = decoder(lat_gen)
-                            images_redec, _, _ = decoder(lat_gen, img_init=images_dec)
-                            z_dec, _, _ = encoder(images_redec, labels)
+                        lat_gen = generator(z_gen, labels)
+                        images_dec, _, _ = decoder(lat_gen)
+                        z_dec, _, _ = encoder(images_redec, labels)
 
-                            loss_gen_dec = (1 / batch_mult) * kl_factor * age_gaussian_kl_loss(F.normalize(z_dec, dim=1))
-                            model_manager.loss_backward(loss_gen_dec, nets_to_train)
-                            loss_gen_dec_sum += loss_gen_dec.item()
-                        else:
-                            z_enc, _, _ = encoder(images, labels)
-                            lat_enc = generator(z_enc, labels)
-                            images_redec, _, _ = decoder(lat_enc, img_init=images)
-                            z_dec, _, _ = encoder(images_redec, labels)
+                        loss_gen_dec = (1 / batch_mult) * kl_factor * age_gaussian_kl_loss(F.normalize(z_dec, dim=1))
+                        model_manager.loss_backward(loss_gen_dec, nets_to_train)
+                        loss_gen_dec_sum += loss_gen_dec.item()
 
-                            loss_gen_dec = (1 / batch_mult) * kl_factor * age_gaussian_kl_loss(F.normalize(z_dec, dim=1))
-                            model_manager.loss_backward(loss_gen_dec, nets_to_train)
-                            loss_gen_dec_sum += loss_gen_dec.item()
+                        # z_enc, _, _ = encoder(images, labels)
+                        # lat_enc = generator(z_enc, labels)
+                        # images_redec, _, _ = decoder(lat_enc, img_init=images)
+                        # z_dec, _, _ = encoder(images_redec, labels)
+                        #
+                        # loss_gen_dec = (1 / batch_mult) * kl_factor * age_gaussian_kl_loss(F.normalize(z_dec, dim=1))
+                        # model_manager.loss_backward(loss_gen_dec, nets_to_train)
+                        # loss_gen_dec_sum += loss_gen_dec.item()
 
                 # AE step
                 with model_manager.on_step(['encoder', 'decoder', 'generator']) as nets_to_train:
