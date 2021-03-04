f044e44feb3e6a67b51c31aa3aad81383b57c678
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-03-04 21:14:20 +0100

    made a few changes to the rage model

--- train_rage.py
+++ train_rage.py
@@ -185,9 +185,9 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                         with torch.no_grad():
                             lat_gen = generator(z_gen, labels)
                             images_dec, _, _ = decoder(lat_gen)
-                            images_redec, _, _ = decoder(lat_gen, img_init=images_dec)
+                            # images_redec, _, _ = decoder(lat_gen, img_init=images_dec)
 
-                        z_redec, _, _ = encoder(images_redec, labels)
+                        z_redec, _, _ = encoder(images_dec, labels)
 
                         loss_dis_dec = (1 / batch_mult) * kl_factor * -age_gaussian_kl_loss(F.normalize(z_redec, dim=1))
                         model_manager.loss_backward(loss_dis_dec, nets_to_train)
@@ -213,8 +213,8 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
 
                             lat_gen = generator(z_gen, labels)
                             images_dec, _, _ = decoder(lat_gen)
-                            images_redec, _, _ = decoder(lat_gen, img_init=images_dec)
-                            z_redec, _, _ = encoder(images_redec, labels)
+                            # images_redec, _, _ = decoder(lat_gen, img_init=images_dec)
+                            z_redec, _, _ = encoder(images_dec, labels)
 
                             loss_gen_dec = (1 / batch_mult) * kl_factor * age_gaussian_kl_loss(F.normalize(z_redec, dim=1))
                             model_manager.loss_backward(loss_gen_dec, nets_to_train)
@@ -222,20 +222,20 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
 
                             z_enc, _, _ = encoder(images, labels)
                             lat_enc = generator(z_enc, labels)
-                            images_redec, _, _ = decoder(lat_enc, img_init=images)
-                            z_redec, _, _ = encoder(images_redec, labels)
+                            images_dec, _, _ = decoder(lat_enc)
+                            z_redec, _, _ = encoder(images_dec, labels)
 
                             loss_enc = (1 / batch_mult) * (2 - z_enc.mul(F.normalize(z_redec, dim=1)).mean())
                             model_manager.loss_backward(loss_enc, nets_to_train)
                             loss_enc_sum += loss_enc.item()
 
-                            z_enc, _, _ = encoder(images, labels)
-                            lat_enc = generator(z_enc, labels)
-                            images_dec, _, _ = decoder(lat_enc)
-
-                            loss_dec = (1 / batch_mult) * F.mse_loss(images_dec, images)
-                            model_manager.loss_backward(loss_dec, nets_to_train)
-                            loss_dec_sum += loss_dec.item()
+                            # z_enc, _, _ = encoder(images, labels)
+                            # lat_enc = generator(z_enc, labels)
+                            # images_dec, _, _ = decoder(lat_enc)
+                            #
+                            # loss_dec = (1 / batch_mult) * F.mse_loss(images_dec, images)
+                            # model_manager.loss_backward(loss_dec, nets_to_train)
+                            # loss_dec_sum += loss_dec.item()
 
                 # Streaming Images
                 with torch.no_grad():
