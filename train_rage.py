20a9e610357cacf67aea7cadbec5ef2fa2d7f26a
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-03-05 08:12:36 +0100

    made a few changes to the rage model

--- train_rage.py
+++ train_rage.py
@@ -229,13 +229,13 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                             model_manager.loss_backward(loss_enc, nets_to_train)
                             loss_enc_sum += loss_enc.item()
 
-                            # z_enc, _, _ = encoder(images, labels)
-                            # lat_enc = generator(z_enc, labels)
-                            # images_dec, _, _ = decoder(lat_enc)
-                            #
-                            # loss_dec = (1 / batch_mult) * F.mse_loss(images_dec, images)
-                            # model_manager.loss_backward(loss_dec, nets_to_train)
-                            # loss_dec_sum += loss_dec.item()
+                            z_enc, _, _ = encoder(images, labels)
+                            lat_enc = generator(z_enc, labels)
+                            images_dec, _, _ = decoder(lat_enc)
+
+                            loss_dec = (1 / batch_mult) * F.mse_loss(images_dec, images)
+                            model_manager.loss_backward(loss_dec, nets_to_train)
+                            loss_dec_sum += loss_dec.item()
 
                 # Streaming Images
                 with torch.no_grad():
