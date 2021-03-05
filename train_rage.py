da4e53c7e99afebbeb8dbf2b48aed91305772602
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-03-05 09:55:18 +0100

    made a few changes to the rage model

--- train_rage.py
+++ train_rage.py
@@ -223,8 +223,9 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                             z_enc, _, _ = encoder(images, labels)
                             lat_enc = generator(z_enc, labels)
                             images_dec, _, _ = decoder(lat_enc)
+                            z_dec, _, _ = encoder(images_dec, labels)
 
-                            loss_dec = (1 / batch_mult) * F.mse_loss(images_dec, images)
+                            loss_dec = (1 / batch_mult) * (2 - F.normalize(z_dec, dim=1).mul(F.normalize(z_enc, dim=1)).mean())
                             model_manager.loss_backward(loss_dec, nets_to_train, retain_graph=True)
                             loss_dec_sum += loss_dec.item()
 
