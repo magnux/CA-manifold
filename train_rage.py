7328484e045d0f5ce9f3bc6acea6ea02df35470b
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-03-07 00:50:10 +0100

    made a few changes to the rage model

--- train_rage.py
+++ train_rage.py
@@ -39,6 +39,7 @@ batch_split_size = batch_size // batch_split
 n_workers = config['training']['n_workers']
 pre_train = config['training']['pre_train'] if 'pre_train' in config['training'] else False
 kl_factor = config['training']['kl_factor'] if 'kl_factor' in config['training'] else 1.
+ae_steps = config['training']['ae_steps'] if 'ae_steps' in config['training'] else 2
 
 # Inputs
 trainset = get_dataset(name=config['data']['name'], type=config['data']['type'],
@@ -170,7 +171,7 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                 loss_gen_enc_sum, loss_gen_dec_sum = 0, 0
                 loss_enc_sum, loss_dec_sum = 0, 0
 
-                # Discriminator step
+                # Discriminator steps
                 with model_manager.on_step(['encoder']) as nets_to_train:
 
                     for _ in range(batch_mult):
@@ -202,7 +203,7 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
 
                         clip_grad_norm_(encoder.parameters(), 0.5, torch._six.inf)
 
-                # Generator step
+                # Generator steps
                 with model_manager.on_step(['decoder', 'generator']) as nets_to_train:
 
                     for _ in range(batch_mult):
@@ -233,27 +234,28 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
 
                         clip_grad_norm_(encoder.parameters(), 0.5, torch._six.inf)
 
-                # AE step
-                with model_manager.on_step(['encoder', 'decoder', 'generator']) as nets_to_train:
+                # AE steps
+                for _ in range(ae_steps):
+                    with model_manager.on_step(['encoder', 'decoder', 'generator']) as nets_to_train:
 
-                    for _ in range(batch_mult):
-                        images, labels, z_gen, trainiter = get_inputs(trainiter, batch_split_size, device)
+                        for _ in range(batch_mult):
+                            images, labels, z_gen, trainiter = get_inputs(trainiter, batch_split_size, device)
 
-                        z_enc, _, _ = encoder(images, labels)
-                        lat_enc = generator(z_enc, labels)
-                        images_dec, _, _ = decoder(lat_enc)
+                            z_enc, _, _ = encoder(images, labels)
+                            lat_enc = generator(z_enc, labels)
+                            images_dec, _, _ = decoder(lat_enc)
 
-                        loss_dec = (1 / batch_mult) * F.mse_loss(images_dec, images)
-                        model_manager.loss_backward(loss_dec, nets_to_train)
-                        loss_dec_sum += loss_dec.item()
+                            loss_dec = (1 / batch_mult) * F.mse_loss(images_dec, images)
+                            model_manager.loss_backward(loss_dec, nets_to_train)
+                            loss_dec_sum += loss_dec.item()
 
-                        # lat_gen = generator(z_gen, labels)
-                        # images_dec, _, _ = decoder(lat_gen)
-                        # z_dec, _, _ = encoder(images_dec, labels)
-                        #
-                        # loss_enc = (1 / batch_mult) * F.mse_loss(z_dec, z_gen)
-                        # model_manager.loss_backward(loss_enc, nets_to_train)
-                        # loss_enc_sum += loss_enc.item()
+                            # lat_gen = generator(z_gen, labels)
+                            # images_dec, _, _ = decoder(lat_gen)
+                            # z_dec, _, _ = encoder(images_dec, labels)
+                            #
+                            # loss_enc = (1 / batch_mult) * F.mse_loss(z_dec, z_gen)
+                            # model_manager.loss_backward(loss_enc, nets_to_train)
+                            # loss_enc_sum += loss_enc.item()
 
                 # Streaming Images
                 with torch.no_grad():
