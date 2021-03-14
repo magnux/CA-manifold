b91993a0e28508e5bdb2abcb15104f37279045d0
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-03-14 10:57:53 +0100

    trying to fix rage again

--- train_rage.py
+++ train_rage.py
@@ -88,6 +88,7 @@ def get_inputs(trainiter, batch_size, device):
     if batch_size % config['training']['batch_size'] > 0:
         images, labels = images[:batch_size, ...], labels[:batch_size, ...]
     images, labels = images.to(device), labels.to(device)
+    labels = labels.detach().requires_grad_()
     images = images.detach().requires_grad_()
     z_gen = zdist.sample((images.size(0),))
     z_gen.detach_().requires_grad_()
