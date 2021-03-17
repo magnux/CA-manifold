f14213ef889eecd2c3c0a2b76003c0003167ec71
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-03-17 17:07:35 +0100

    trying to fix rage again

--- train_rage.py
+++ train_rage.py
@@ -259,9 +259,8 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
 
                         lat_enc = generator(z_enc, labels)
                         images_dec, out_embs, _ = decoder(lat_enc)
-                        z_dec, _, _ = encoder(images_dec, labels)
 
-                        loss_dec = (1 / batch_mult) * F.mse_loss(z_dec, z_enc)
+                        loss_dec = (1 / batch_mult) * F.mse_loss(images_dec, images)
                         model_manager.loss_backward(loss_dec, nets_to_train, retain_graph=True)
                         loss_dec_sum += loss_dec.item()
 
