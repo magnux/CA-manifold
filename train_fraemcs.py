87052519565831362a2801a648f452ed1469f6f6
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-03-26 19:47:01 +0100

    added augment layer to frae

--- train_fraemcs.py
+++ train_fraemcs.py
@@ -267,9 +267,9 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                         lat_enc = generator(z_enc)
                         images_dec, _, _ = decoder(lat_enc, seed_n=seed_idx[n_labels, it % n_seed].tolist())
 
-                        loss_dec = (1 / batch_mult) * reg_dis_enc_sum * F.mse_loss(images_dec, images)
+                        loss_dec = (1 / batch_mult) * np.sqrt(reg_dis_enc_sum) * F.mse_loss(images_dec, images)
                         model_manager.loss_backward(loss_dec, nets_to_train, retain_graph=True)
-                        loss_dec_sum += loss_dec.item()
+                        loss_dec_sum += loss_dec.item() / np.sqrt(reg_dis_enc_sum)
 
                         lat_top_enc, _, _ = dis_encoder(images, lat_enc)
                         labs_enc = discriminator(lat_top_enc, labels)
