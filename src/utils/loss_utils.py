9d3368d4d58de65e29b39dd804950107f54dc01a
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-03-30 09:33:59 +0200

    testing something with frae

--- src/utils/loss_utils.py
+++ src/utils/loss_utils.py
@@ -134,7 +134,7 @@ def compute_pl_reg(g_out, g_in, pl_mean, beta=0.99, alt_pl=None, reg_factor=1., 
     return pl_reg, new_pl_mean
 
 
-def update_ada_augment_p(current_p, logits_sign_mean, batch_size, ada_target=0.1, kimgs=5e5):
+def update_ada_augment_p(current_p, logits_sign_mean, batch_size, ada_target=0.2, kimgs=5e5):
     adjust = np.sign(logits_sign_mean - ada_target) * batch_size / kimgs
     return F.relu(current_p + adjust)
 
