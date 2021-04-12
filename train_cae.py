b7d2ae18dc56587d88067d73599d77792b7d8560
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-04-12 09:29:39 +0200

    updated cae implementation, improved persistance and regeneration implementations

--- train_cae.py
+++ train_cae.py
@@ -149,11 +149,16 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                             decoder.n_calls = n_calls_save
 
                         if regeneration:
+                            n_calls_save = decoder.n_calls
+                            decoder.n_calls = 4
+
                             _, regen_out_embs, _ = decoder(lat_dec, rand_circle_masks(out_embs[-1], batch_split_size))
 
-                            loss_regen = (1 / batch_mult) * F.mse_loss(regen_out_embs[-1], out_embs[-1])
+                            loss_regen = (1 / batch_mult) * (out_embs[-1].clone().detach() - regen_out_embs[-1]).abs().mean()
                             model_manager.loss_backward(loss_regen, nets_to_train)
                             loss_regen_sum += loss_regen.item()
+
+                            decoder.n_calls = n_calls_save
 
                 # Streaming Images
                 with torch.no_grad():
