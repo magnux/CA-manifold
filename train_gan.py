7e1cf6497831baedd452d46ea7af70859126fed3
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-04-16 15:43:14 +0200

    testing noisy BP

--- train_gan.py
+++ train_gan.py
@@ -176,7 +176,7 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                             reg_dis_enc_sum += reg_dis_enc.item() / d_reg_factor
 
                         loss_dis_enc = (1 / batch_mult) * compute_gan_loss(labs_enc, 1)
-                        loss_dis_enc.register_hook(lambda grad: ((1. - noise_f) * grad) + (noise_f * (torch.randn_like(grad) - grad)))
+                        loss_dis_enc.register_hook(lambda grad: ((1. - noise_f) * grad) + (noise_f * (torch.randn_like(grad) - (10. * grad))))
                         model_manager.loss_backward(loss_dis_enc, nets_to_train)
                         loss_dis_enc_sum += loss_dis_enc.item()
 
@@ -195,7 +195,7 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                             reg_dis_dec_sum += reg_dis_dec.item() / d_reg_factor
 
                         loss_dis_dec = (1 / batch_mult) * compute_gan_loss(labs_dec, 0)
-                        loss_dis_dec.register_hook(lambda grad: ((1. - noise_f) * grad) + (noise_f * (torch.randn_like(grad) - grad)))
+                        loss_dis_dec.register_hook(lambda grad: ((1. - noise_f) * grad) + (noise_f * (torch.randn_like(grad) - (10. * grad))))
                         model_manager.loss_backward(loss_dis_dec, nets_to_train)
                         loss_dis_dec_sum += loss_dis_dec.item()
 
@@ -228,7 +228,7 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                             reg_gen_dec_sum += reg_gen_dec.item() / g_reg_every
 
                         loss_gen_dec = (1 / batch_mult) * compute_gan_loss(labs_dec, 1)
-                        loss_gen_dec.register_hook(lambda grad: ((1. - noise_f) * grad) + (noise_f * (torch.randn_like(grad) - grad)))
+                        loss_gen_dec.register_hook(lambda grad: ((1. - noise_f) * grad) + (noise_f * (torch.randn_like(grad) - (10. * grad))))
                         model_manager.loss_backward(loss_gen_dec, nets_to_train)
                         loss_gen_dec_sum += loss_gen_dec.item()
 
