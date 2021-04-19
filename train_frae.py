0faec6604dc818bf90361bf473436fddc4a2ec1e
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-04-19 09:12:36 +0200

    testing noisy BP

--- train_frae.py
+++ train_frae.py
@@ -230,7 +230,7 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                             reg_dis_enc_sum += reg_dis_enc.item() / d_reg_factor
 
                         loss_dis_enc = (1 / batch_mult) * compute_gan_loss(labs_enc, 1)
-                        lat_top_enc.register_hook(lambda grad: ((1. - noise_f) * grad) + (noise_f * (torch.rand_like(grad) - grad)))
+                        lat_top_enc.register_hook(lambda grad: ((1. - noise_f) * grad) + (noise_f * (1e-3 * torch.rand_like(grad) - grad)))
                         model_manager.loss_backward(loss_dis_enc, nets_to_train)
                         loss_dis_enc_sum += loss_dis_enc.item()
 
@@ -257,7 +257,7 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                             reg_dis_dec_sum += reg_dis_dec.item() / d_reg_factor
 
                         loss_dis_dec = (1 / batch_mult) * compute_gan_loss(labs_dec, 0)
-                        lat_top_dec.register_hook(lambda grad: ((1. - noise_f) * grad) + (noise_f * (torch.rand_like(grad) - grad)))
+                        lat_top_dec.register_hook(lambda grad: ((1. - noise_f) * grad) + (noise_f * (1e-3 * torch.rand_like(grad) - grad)))
                         model_manager.loss_backward(loss_dis_dec, nets_to_train)
                         loss_dis_dec_sum += loss_dis_dec.item()
 
@@ -287,7 +287,7 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                         labs_enc = discriminator(lat_top_enc, labels)
 
                         loss_gen_enc = (1 / batch_mult) * compute_gan_loss(labs_enc, 0)
-                        lat_top_enc.register_hook(lambda grad: ((1. - noise_f) * grad) + (noise_f * (torch.rand_like(grad) - grad)))
+                        lat_top_enc.register_hook(lambda grad: ((1. - noise_f) * grad) + (noise_f * (1e-3 * torch.rand_like(grad) - grad)))
                         model_manager.loss_backward(loss_gen_enc, nets_to_train)
                         loss_gen_enc_sum += loss_gen_enc.item()
 
@@ -306,7 +306,7 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                         labs_dec = discriminator(lat_top_dec, labels)
 
                         loss_gen_dec = (1 / batch_mult) * compute_gan_loss(labs_dec, 1)
-                        lat_top_dec.register_hook(lambda grad: ((1. - noise_f) * grad) + (noise_f * (torch.rand_like(grad) - grad)))
+                        lat_top_dec.register_hook(lambda grad: ((1. - noise_f) * grad) + (noise_f * (1e-3 * torch.rand_like(grad) - grad)))
                         model_manager.loss_backward(loss_gen_dec, nets_to_train)
                         loss_gen_dec_sum += loss_gen_dec.item()
 
