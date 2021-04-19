79e8d81da394550b3425716750542b5c9b6d4109
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-04-20 00:24:33 +0200

    testing with different hooks

--- train_gaems.py
+++ train_gaems.py
@@ -225,8 +225,8 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                         labs_dec = discriminator(lat_top_dec, labels)
                         labs_dis_dec_sign -= ((1 / batch_mult) * labs_dec.sign().mean()).item()
 
-                g_factor_enc = np.clip(g_factor_enc + 1e-2 * (labs_dis_enc_sign - 0.2), 0., 1.)
-                g_factor_dec = np.clip(g_factor_dec + 1e-2 * (labs_dis_dec_sign - 0.0), 0., 1.)
+                g_factor_enc = np.clip(g_factor_enc + 1e-4 * (labs_dis_enc_sign - 0.2), 0., 1.)
+                g_factor_dec = np.clip(g_factor_dec + 1e-4 * (labs_dis_dec_sign - 0.0), 0., 1.)
 
                 with model_manager.on_step(['dis_encoder', 'discriminator']) as nets_to_train:
 
