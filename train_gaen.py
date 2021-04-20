956ffdff18132c1dbcb291a66857130b0d480f0b
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-04-20 10:57:20 +0200

    testing with different hooks

--- train_gaen.py
+++ train_gaen.py
@@ -8,7 +8,7 @@ from tqdm import trange
 from src.config import load_config
 from src.distributions import get_ydist, get_zdist
 from src.inputs import get_dataset
-from src.utils.loss_utils import compute_grad_reg, compute_gan_loss, update_reg_params
+from src.utils.loss_utils import compute_grad_reg, compute_gan_loss, update_reg_params, compute_pl_reg
 from src.utils.model_utils import compute_inception_score
 from src.model_manager import ModelManager
 from src.utils.web.webstreaming import stream_images
@@ -308,11 +308,11 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                         lat_top_enc, _, _ = dis_encoder(images, lat_enc)
                         labs_enc = discriminator(lat_top_enc, labels)
 
-                        # if g_reg_every > 0 and it % g_reg_every == 0:
-                        #     reg_gen_enc, pl_mean_enc = compute_pl_reg(lat_enc, images, pl_mean_enc)
-                        #     reg_gen_enc = (1 / batch_mult) * g_reg_every * reg_gen_enc
-                        #     model_manager.loss_backward(reg_gen_enc, nets_to_train, retain_graph=True)
-                        #     reg_gen_enc_sum += reg_gen_enc.item() / g_reg_every
+                        if g_reg_every > 0 and it % g_reg_every == 0:
+                            reg_gen_enc, pl_mean_enc = compute_pl_reg(lat_enc, images, pl_mean_enc)
+                            reg_gen_enc = (1 / batch_mult) * g_reg_every * reg_gen_enc
+                            model_manager.loss_backward(reg_gen_enc, nets_to_train, retain_graph=True)
+                            reg_gen_enc_sum += reg_gen_enc.item() / g_reg_every
 
                         loss_gen_enc = (1 / batch_mult) * compute_gan_loss(labs_enc, 0)
                         # lat_top_enc.register_hook(lambda grad: ((1. - g_factor_enc) * grad) + (g_factor_enc * (grad.std(dim=1).mean() * torch.randn_like(grad) - grad)))
@@ -339,11 +339,11 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                         lat_top_dec, _, _ = dis_encoder(images_dec, lat_gen)
                         labs_dec = discriminator(lat_top_dec, labels)
 
-                        # if g_reg_every > 0 and it % g_reg_every == 0:
-                        #     reg_gen_dec, pl_mean_dec = compute_pl_reg(images_dec, lat_gen, pl_mean_dec)
-                        #     reg_gen_dec = (1 / batch_mult) * g_reg_every * reg_gen_dec
-                        #     model_manager.loss_backward(reg_gen_dec, nets_to_train, retain_graph=True)
-                        #     reg_gen_dec_sum += reg_gen_dec.item() / g_reg_every
+                        if g_reg_every > 0 and it % g_reg_every == 0:
+                            reg_gen_dec, pl_mean_dec = compute_pl_reg(images_dec, lat_gen, pl_mean_dec)
+                            reg_gen_dec = (1 / batch_mult) * g_reg_every * reg_gen_dec
+                            model_manager.loss_backward(reg_gen_dec, nets_to_train, retain_graph=True)
+                            reg_gen_dec_sum += reg_gen_dec.item() / g_reg_every
 
                         loss_gen_dec = (1 / batch_mult) * compute_gan_loss(labs_dec, 1)
                         # lat_top_dec.register_hook(lambda grad: ((1. - g_factor_dec) * grad) + (g_factor_dec * (grad.std(dim=1).mean() * torch.randn_like(grad) - grad)))
@@ -391,12 +391,12 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                 model_manager.log_manager.add_scalar('regs', 'd_reg_every_mean', d_reg_every_mean, it=it)
                 model_manager.log_manager.add_scalar('regs', 'd_reg_param_mean', d_reg_param_mean, it=it)
 
-                # if g_reg_every > 0:
-                #     model_manager.log_manager.add_scalar('regs', 'reg_gen_enc', reg_gen_enc_sum, it=it)
-                #     model_manager.log_manager.add_scalar('regs', 'reg_gen_dec', reg_gen_dec_sum, it=it)
-                #
-                #     model_manager.log_manager.add_scalar('regs', 'pl_mean_enc', pl_mean_enc, it=it)
-                #     model_manager.log_manager.add_scalar('regs', 'pl_mean_dec', pl_mean_dec, it=it)
+                if g_reg_every > 0:
+                    model_manager.log_manager.add_scalar('regs', 'reg_gen_enc', reg_gen_enc_sum, it=it)
+                    model_manager.log_manager.add_scalar('regs', 'reg_gen_dec', reg_gen_dec_sum, it=it)
+
+                    model_manager.log_manager.add_scalar('regs', 'pl_mean_enc', pl_mean_enc, it=it)
+                    model_manager.log_manager.add_scalar('regs', 'pl_mean_dec', pl_mean_dec, it=it)
 
                 # model_manager.log_manager.add_scalar('norms', 'dis_grad_norm', dis_grad_norm, it=it)
                 # model_manager.log_manager.add_scalar('norms', 'dis_enc_grad_norm', dis_enc_grad_norm, it=it)
