2fadaf24bcb9c59cc451ba6f56eafe286b1b2244
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-04-23 08:14:10 +0200

    testing with newer hook, all the way

--- train_gan.py
+++ train_gan.py
@@ -202,12 +202,12 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                         model_manager.loss_backward(loss_dis_dec, nets_to_train)
                         loss_dis_dec_sum += loss_dis_dec.item()
 
-                    if d_reg_every_mean > 0 and it % d_reg_every_mean == 0:
-                        reg_dis_mean = 0.5 * (reg_dis_enc_sum + reg_dis_dec_sum)
-                        loss_dis_mean = 0.5 * (loss_dis_enc_sum + loss_dis_dec_sum)
-                        d_reg_every_mean = d_reg_every_mean_next
-                        d_reg_every_mean_next, d_reg_param_mean = update_reg_params(d_reg_every_mean_next, d_reg_every, d_reg_param_mean,
-                                                                                    reg_dis_mean, reg_dis_target, loss_dis_mean)
+                    # if d_reg_every_mean > 0 and it % d_reg_every_mean == 0:
+                    #     reg_dis_mean = 0.5 * (reg_dis_enc_sum + reg_dis_dec_sum)
+                    #     loss_dis_mean = 0.5 * (loss_dis_enc_sum + loss_dis_dec_sum)
+                    #     d_reg_every_mean = d_reg_every_mean_next
+                    #     d_reg_every_mean_next, d_reg_param_mean = update_reg_params(d_reg_every_mean_next, d_reg_every, d_reg_param_mean,
+                    #                                                                 reg_dis_mean, reg_dis_target, loss_dis_mean)
 
                     g_factor_enc = np.clip(g_factor_enc + 1e-2 * (labs_dis_enc_sign - 0.1), 1., 1e3)
                     g_factor_dec = np.clip(g_factor_dec + 1e-2 * (labs_dis_dec_sign - 0.1), 1., 1e3)
