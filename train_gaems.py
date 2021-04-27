1f3e76a192f2d704507e3fa53a2581616b7e4ea4
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-04-27 18:13:01 +0200

    testing simpler gen

--- train_gaems.py
+++ train_gaems.py
@@ -232,7 +232,7 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                             reg_dis_enc_sum += reg_dis_enc.item() / d_reg_factor
 
                         loss_dis_enc = (1 / batch_mult) * compute_gan_loss(labs_enc, 1)
-                        labs_enc.register_hook(grad_mult_hook(g_factor_enc))
+                        # labs_enc.register_hook(grad_mult_hook(g_factor_enc))
                         model_manager.loss_backward(loss_dis_enc, nets_to_train)
                         loss_dis_enc_sum += loss_dis_enc.item()
 
@@ -282,7 +282,7 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                         labs_enc = discriminator(lat_top_enc, labels)
 
                         loss_gen_enc = (1 / batch_mult) * compute_gan_loss(labs_enc, 0)
-                        lat_top_enc.register_hook(grad_noise_hook(1, 2))
+                        # lat_top_enc.register_hook(grad_noise_hook(1, 2))
                         model_manager.loss_backward(loss_gen_enc, nets_to_train)
                         loss_gen_enc_sum += loss_gen_enc.item()
 
@@ -293,7 +293,7 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                         labs_dec = discriminator(lat_top_dec, labels)
 
                         loss_gen_dec = (1 / batch_mult) * compute_gan_loss(labs_dec, 1)
-                        lat_top_dec.register_hook(grad_noise_hook(1, 2))
+                        # lat_top_dec.register_hook(grad_noise_hook(1, 2))
                         model_manager.loss_backward(loss_gen_dec, nets_to_train)
                         loss_gen_dec_sum += loss_gen_dec.item()
 
