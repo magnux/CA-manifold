2625692cf340942f8913f46a4d9c3d3f69216964
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-04-29 11:30:35 +0200

    testing linear + dynalinear model

--- train_frae.py
+++ train_frae.py
@@ -249,14 +249,14 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                         labs_dec = discriminator(lat_top_dec, labels)
                         labs_dis_dec_sign -= ((1 / batch_mult) * labs_dec.sign().mean()).item()
 
-                        # if d_reg_every_mean > 0 and it % d_reg_every_mean == 0:
-                        #     reg_dis_dec = (1 / batch_mult) * d_reg_factor * compute_grad_reg(labs_dec, images_redec)
-                        #     model_manager.loss_backward(reg_dis_dec, nets_to_train, retain_graph=True)
-                        #     reg_dis_dec_sum += reg_dis_dec.item() / d_reg_factor
-                        #
-                        #     reg_dis_dec = (1 / batch_mult) * d_reg_factor * compute_grad_reg(labs_dec, lat_gen)
-                        #     model_manager.loss_backward(reg_dis_dec, nets_to_train, retain_graph=True)
-                        #     reg_dis_dec_sum += reg_dis_dec.item() / d_reg_factor
+                        if d_reg_every_mean > 0 and it % d_reg_every_mean == 1:
+                            reg_dis_dec = (1 / batch_mult) * d_reg_factor * compute_grad_reg(labs_dec, images_redec)
+                            model_manager.loss_backward(reg_dis_dec, nets_to_train, retain_graph=True)
+                            reg_dis_dec_sum += reg_dis_dec.item() / d_reg_factor
+
+                            reg_dis_dec = (1 / batch_mult) * d_reg_factor * compute_grad_reg(labs_dec, lat_gen)
+                            model_manager.loss_backward(reg_dis_dec, nets_to_train, retain_graph=True)
+                            reg_dis_dec_sum += reg_dis_dec.item() / d_reg_factor
 
                         loss_dis_dec = (1 / batch_mult) * compute_gan_loss(labs_dec, 0)
                         labs_dec.register_hook(grad_mult_hook(g_factor_dec))
