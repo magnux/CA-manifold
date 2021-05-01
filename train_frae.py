ca3fa2393e64635aac8d847209d7a31d9fe3a066
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-05-01 11:56:05 +0200

    testing dual gen loss

--- train_frae.py
+++ train_frae.py
@@ -293,6 +293,7 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
 
                         loss_gen_enc = (1 / batch_mult) * compute_gan_loss(labs_enc, 0)
                         loss_gen_enc -= (1 / batch_mult) * 0.1 * compute_gan_loss(labs_enc, 1)
+                        labs_enc.register_hook(grad_mult_hook(g_factor_dec))
                         model_manager.loss_backward(loss_gen_enc, nets_to_train)
                         loss_gen_enc_sum += loss_gen_enc.item()
 
@@ -312,6 +313,7 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
 
                         loss_gen_dec = (1 / batch_mult) * compute_gan_loss(labs_dec, 1)
                         loss_gen_dec -= (1 / batch_mult) * 0.1 * compute_gan_loss(labs_dec, 0)
+                        labs_dec.register_hook(grad_mult_hook(g_factor_enc))
                         model_manager.loss_backward(loss_gen_dec, nets_to_train)
                         loss_gen_dec_sum += loss_gen_dec.item()
 
--- train_gaems.py
+++ train_gaems.py
@@ -285,6 +285,7 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
 
                         loss_gen_enc = (1 / batch_mult) * compute_gan_loss(labs_enc, 0)
                         loss_gen_enc -= (1 / batch_mult) * 0.1 * compute_gan_loss(labs_enc, 1)
+                        labs_enc.register_hook(grad_mult_hook(g_factor_dec))
                         model_manager.loss_backward(loss_gen_enc, nets_to_train)
                         loss_gen_enc_sum += loss_gen_enc.item()
 
@@ -296,6 +297,7 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
 
                         loss_gen_dec = (1 / batch_mult) * compute_gan_loss(labs_dec, 1)
                         loss_gen_dec -= (1 / batch_mult) * 0.1 * compute_gan_loss(labs_dec, 0)
+                        labs_dec.register_hook(grad_mult_hook(g_factor_enc))
                         model_manager.loss_backward(loss_gen_dec, nets_to_train)
                         loss_gen_dec_sum += loss_gen_dec.item()
 
