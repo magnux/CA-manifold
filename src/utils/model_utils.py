cc4001644e8418ed7c70976f041df780154f3a14
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-05-14 16:49:16 +0200

    testing... testing...

--- src/utils/model_utils.py
+++ src/utils/model_utils.py
@@ -42,39 +42,38 @@ def clip_grad_ind_norm(network, max_norm=1., norm_type=torch._six.inf):
             p.grad.data.copy_(p.grad.data.max(norm))
 
 
-def grad_noise(network, p_factor, momentum=0.9):
+def grad_noise(network, g_factor, momentum=0.9):
     for p in network.parameters():
         if p.grad is not None:
             if not hasattr(p, 'rand_grad'):
                 p.rand_grad = F.normalize(torch.randn_like(p.grad), dim=-1)
             else:
                 p.rand_grad = F.normalize(momentum * p.rand_grad + (1. - momentum) * torch.randn_like(p.grad), dim=-1)
-            p.grad.data.copy_((1. - p_factor) * p.grad + p_factor * (p.rand_grad * p.grad.norm(2) - p.grad))
+            p.grad.data.copy_((1. - g_factor) * p.grad + g_factor * (p.rand_grad * p.grad.norm(2) - p.grad))
 
 
-def grad_noise_hook(p_factor, g_factor):
-    def _grad_noise_hook(grad, p_factor=1, g_factor=1):
+def grad_noise_hook(g_factor):
+    def _grad_noise_hook(grad, g_factor=1):
         with torch.no_grad():
             noisy_grad = grad.clone()
             # ridx = np.random.randint(0, grad.shape[1])
             # sel_idxs = [i for i in range(ridx)] + [i for i in range(ridx + 1, grad.shape[1])]
             # noisy_grad[:, ridx] = -(grad[:, sel_idxs] * noisy_grad[:, sel_idxs]).sum(1) / (grad[:, ridx] + 1e-4)
             noisy_grad *= torch.rand_like(grad)
-            noisy_grad *= g_factor * torch.rand([grad.shape[0], 1], device=grad.device)
-            return (1 - p_factor) * grad + p_factor * noisy_grad
-    return partial(_grad_noise_hook, p_factor=p_factor, g_factor=g_factor)
+            return (1 - g_factor) * grad + g_factor * noisy_grad
+    return partial(_grad_noise_hook, g_factor=g_factor)
 
 
 def grad_mult(network, g_factor):
     for p in network.parameters():
         if p.grad is not None:
-            p.grad.data.copy_(g_factor * p.grad + (1 - g_factor) * torch.rand_like(p.grad) * p.grad)
+            p.grad.data.copy_(g_factor * p.grad)
 
 
 def grad_mult_hook(g_factor):
     def _grad_mult_hook(grad, g_factor=1):
         with torch.no_grad():
-            return g_factor * grad + (1 - g_factor) * torch.rand_like(grad) * grad
+            return g_factor * grad
     return partial(_grad_mult_hook, g_factor=g_factor)
 
 
--- train_gan.py
+++ train_gan.py
@@ -9,7 +9,7 @@ from src.config import load_config
 from src.distributions import get_ydist, get_zdist
 from src.inputs import get_dataset
 from src.utils.loss_utils import compute_gan_loss, compute_grad_reg, compute_pl_reg, update_reg_params, update_g_factors
-from src.utils.model_utils import compute_inception_score, grad_mult, grad_mult_hook
+from src.utils.model_utils import compute_inception_score, grad_mult, grad_noise_hook
 from src.model_manager import ModelManager
 from src.utils.web.webstreaming import stream_images
 from os.path import basename, splitext
@@ -179,7 +179,7 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                             reg_dis_enc_sum += reg_dis_enc.item() / d_reg_factor
 
                         loss_dis_enc = (1 / batch_mult) * compute_gan_loss(labs_enc, 1)
-                        # labs_enc.register_hook(grad_mult_hook(g_factor_enc))
+                        labs_enc.register_hook(grad_noise_hook(g_factor_enc))
                         model_manager.loss_backward(loss_dis_enc, nets_to_train)
                         loss_dis_enc_sum += loss_dis_enc.item()
 
@@ -199,7 +199,7 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                             reg_dis_dec_sum += reg_dis_dec.item() / d_reg_factor
 
                         loss_dis_dec = (1 / batch_mult) * compute_gan_loss(labs_dec, 0)
-                        # labs_dec.register_hook(grad_mult_hook(g_factor_dec))
+                        labs_dec.register_hook(grad_noise_hook(g_factor_dec))
                         model_manager.loss_backward(loss_dis_dec, nets_to_train)
                         loss_dis_dec_sum += loss_dis_dec.item()
 
@@ -213,8 +213,8 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
 
                     g_factor_enc, g_factor_dec = update_g_factors(g_factor_enc, g_factor_dec, labs_dis_enc_sign, labs_dis_dec_sign, sign_mean_target)
                     # dis_encoder.fire_rate = 0.5 * (g_factor_enc + g_factor_dec)
-                    grad_mult(dis_encoder, 0.5 * (g_factor_enc + g_factor_dec))
-                    grad_mult(discriminator, 0.5 * (g_factor_enc + g_factor_dec))
+                    # grad_mult(dis_encoder, 0.5 * (g_factor_enc + g_factor_dec))
+                    # grad_mult(discriminator, 0.5 * (g_factor_enc + g_factor_dec))
 
                 # Generator step
                 with model_manager.on_step(['decoder', 'generator']) as nets_to_train:
@@ -236,7 +236,7 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                             reg_gen_dec_sum += reg_gen_dec.item() / g_reg_every
 
                         loss_gen_dec = (1 / batch_mult) * compute_gan_loss(labs_dec, 1)
-                        # labs_dec.register_hook(grad_mult_hook(g_factor_enc ** 0.5))
+                        labs_dec.register_hook(grad_noise_hook(g_factor_enc ** 0.5))
                         model_manager.loss_backward(loss_gen_dec, nets_to_train)
                         loss_gen_dec_sum += loss_gen_dec.item()
 
