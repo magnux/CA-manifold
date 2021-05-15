24695a64eda10faf15efdefcfc6cee968092f18d
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-05-15 12:55:43 +0200

    testing... testing...

--- src/utils/loss_utils.py
+++ src/utils/loss_utils.py
@@ -32,7 +32,8 @@ def compute_gan_loss(d_out, target, gan_type='new_mse'):
     elif gan_type == 'softplus':
         loss = F.softplus((1 - 2*target) * d_out).mean()
     elif gan_type == 'new_mse':
-        loss = F.mse_loss(d_out, 2*target - 1)
+        target = d_out.new_full(size=d_out.size(), fill_value=(2*target - 1))
+        loss = F.mse_loss(d_out, target)
     else:
         raise NotImplementedError
 
