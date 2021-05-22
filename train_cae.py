276f3de4c4c1b46912f8604b0a03289447df6a98
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-05-22 15:14:30 +0200

    testing... testing...

--- train_cae.py
+++ train_cae.py
@@ -146,7 +146,7 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
 
                             pers_target_out_embs = [target_out_embs for _ in range(pers_steps)]
 
-                            loss_pers = (1 / batch_mult) * 100 * F.mse_loss(torch.stack(pers_out_embs[1:]), torch.stack(pers_target_out_embs))
+                            loss_pers = (1 / batch_mult) * 10 * F.mse_loss(torch.stack(pers_out_embs[1:]), torch.stack(pers_target_out_embs))
                             model_manager.loss_backward(loss_pers, nets_to_train, retain_graph=True)
                             loss_pers_sum += loss_pers.item()
 
@@ -162,7 +162,7 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
 
                             regen_target_out_embs = [target_out_embs for _ in range(regen_steps)]
 
-                            loss_regen = (1 / batch_mult) * 100 * F.mse_loss(torch.stack(regen_out_embs[1:]), torch.stack(regen_target_out_embs))
+                            loss_regen = (1 / batch_mult) * 10 * F.mse_loss(torch.stack(regen_out_embs[1:]), torch.stack(regen_target_out_embs))
                             model_manager.loss_backward(loss_regen, nets_to_train, retain_graph=True)
                             loss_regen_sum += loss_regen.item()
 
