9f446c51eac32a5fcba105c7d12e9c6749f5d895
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-05-23 15:58:54 +0200

    testing... testing...

--- src/networks/base.py
+++ src/networks/base.py
@@ -26,6 +26,7 @@ class Discriminator(nn.Module):
         self.lat_size = lat_size
         self.fhidden = lat_size if lat_size > 3 else 512
         self.embed_size = embed_size
+        self.lat_bn = nn.InstanceNorm1d(self.lat_size)
         self.register_buffer('lat_bias', torch.randn(16, self.lat_size))
         self.register_buffer('embedding_mat', torch.eye(n_labels))
         self.exp_yembed = nn.Linear(n_labels, self.lat_size, bias=False)
@@ -42,7 +43,7 @@ class Discriminator(nn.Module):
         else:
             yembed = y
 
-        lat = lat + torch.cat([self.lat_bias] * (lat.size(0) // 16), 0)
+        lat = self.lat_norm(lat) + torch.cat([self.lat_bias] * (lat.size(0) // 16), 0)
         score = (self.lat_to_score(lat) * yembed).sum(dim=1, keepdim=True) * (1 / np.sqrt(yembed.shape[1]))
         score = score + self.dyna_lat_to_score(lat, self.exp_yembed(yembed))
 
@@ -119,11 +120,12 @@ class UnconditionalDiscriminator(nn.Module):
     def __init__(self, lat_size, **kwargs):
         super().__init__()
         self.lat_size = lat_size
+        self.lat_norm = nn.InstanceNorm1d(self.lat_size)
         self.register_buffer('lat_bias', torch.randn(16, self.lat_size))
         self.lat_to_score = nn.Linear(self.lat_size, 1, bias=False)
 
     def forward(self, lat):
-        lat = lat + torch.cat([self.lat_bias] * (lat.size(0) // 16), 0)
+        lat = self.lat_norm(lat) + torch.cat([self.lat_bias] * (lat.size(0) // 16), 0)
         score = self.lat_to_score(lat)
 
         return score
