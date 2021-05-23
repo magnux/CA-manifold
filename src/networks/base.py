106f97819fe76736352c42cef7321cc547b6bfbc
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-05-23 16:55:12 +0200

    testing... testing...

--- src/networks/base.py
+++ src/networks/base.py
@@ -21,13 +21,14 @@ class Classifier(nn.Module):
 
 
 class Discriminator(nn.Module):
-    def __init__(self, n_labels, lat_size, embed_size, **kwargs):
+    def __init__(self, n_labels, lat_size, embed_size, auto_reg=True, **kwargs):
         super().__init__()
         self.lat_size = lat_size
         self.fhidden = lat_size if lat_size > 3 else 512
         self.embed_size = embed_size
-        self.lat_bn = nn.BatchNorm1d(self.lat_size)
-        self.register_buffer('lat_bias', torch.rand(1, self.lat_size))
+        self.auto_reg = auto_reg
+
+        self.register_buffer('lat_bias', torch.randn(16, self.lat_size))
         self.register_buffer('embedding_mat', torch.eye(n_labels))
         self.exp_yembed = nn.Linear(n_labels, self.lat_size, bias=False)
         self.dyna_lat_to_score = DynaLinear(self.lat_size, self.lat_size, 1, bias=False)
@@ -43,7 +44,12 @@ class Discriminator(nn.Module):
         else:
             yembed = y
 
-        lat = self.lat_bn(lat) + torch.cat([self.lat_bias] * lat.size(0), 0)
+        if self.auto_reg and lat.requires_grad:
+            with torch.no_grad():
+                auto_reg_grad = (2 / lat.numel()) * lat
+            lat.register_hook(lambda grad: grad + auto_reg_grad)
+
+        lat = lat + torch.cat([self.lat_bias] * (lat.size(0) // 16), 0)
         score = (self.lat_to_score(lat) * yembed).sum(dim=1, keepdim=True) * (1 / np.sqrt(yembed.shape[1]))
         score = score + self.dyna_lat_to_score(lat, self.exp_yembed(yembed))
 
@@ -117,15 +123,21 @@ class LabsEncoder(nn.Module):
 
 
 class UnconditionalDiscriminator(nn.Module):
-    def __init__(self, lat_size, **kwargs):
+    def __init__(self, lat_size, auto_reg=True, **kwargs):
         super().__init__()
         self.lat_size = lat_size
-        self.lat_bn = nn.BatchNorm1d(self.lat_size)
-        self.register_buffer('lat_bias', torch.rand(1, self.lat_size))
+        self.auto_reg = auto_reg
+
+        self.register_buffer('lat_bias', torch.randn(16, self.lat_size))
         self.lat_to_score = nn.Linear(self.lat_size, 1, bias=False)
 
     def forward(self, lat):
-        lat = self.lat_bn(lat) + torch.cat([self.lat_bias] * lat.size(0), 0)
+        if self.auto_reg and lat.requires_grad:
+            with torch.no_grad():
+                auto_reg_grad = (2 / lat.numel()) * lat
+            lat.register_hook(lambda grad: grad + auto_reg_grad)
+
+        lat = lat + torch.cat([self.lat_bias] * (lat.size(0) // 16), 0)
         score = self.lat_to_score(lat)
 
         return score
