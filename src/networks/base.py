776a3e168d0c30bc5c00cfb6350547adb2c4d363
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-05-24 10:36:17 +0200

    testing... testing...

--- src/networks/base.py
+++ src/networks/base.py
@@ -28,7 +28,7 @@ class Discriminator(nn.Module):
         self.embed_size = embed_size
         self.auto_reg = auto_reg
 
-        self.lat_dropout = nn.Dropout(0.5)
+        self.register_buffer('lat_center', torch.zeros(1, self.lat_size))
         self.register_buffer('embedding_mat', torch.eye(n_labels))
         self.exp_yembed = nn.Linear(n_labels, self.lat_size, bias=False)
         self.dyna_lat_to_score = DynaLinear(self.lat_size, self.lat_size, 1, bias=False)
@@ -49,7 +49,10 @@ class Discriminator(nn.Module):
                 auto_reg_grad = (2e-3 / lat.numel()) * lat
             lat.register_hook(lambda grad: grad + auto_reg_grad)
 
-        lat = self.lat_dropout(lat)
+        if lat.requires_grad:
+            self.lat_center.data.copy_(0.9 * self.lat_center + 0.1 * lat.mean(dim=0, keepdim=True))
+
+        lat = lat - self.lat_center
         score = (self.lat_to_score(lat) * yembed).sum(dim=1, keepdim=True) * (1 / np.sqrt(yembed.shape[1]))
         score = score + self.dyna_lat_to_score(lat, self.exp_yembed(yembed))
 
@@ -128,7 +131,7 @@ class UnconditionalDiscriminator(nn.Module):
         self.lat_size = lat_size
         self.auto_reg = auto_reg
 
-        self.lat_dropout = nn.Dropout(0.5)
+        self.register_buffer('lat_center', torch.zeros(1, self.lat_size))
         self.lat_to_score = nn.Linear(self.lat_size, 1, bias=False)
 
     def forward(self, lat):
@@ -137,7 +140,10 @@ class UnconditionalDiscriminator(nn.Module):
                 auto_reg_grad = (2e-3 / lat.numel()) * lat
             lat.register_hook(lambda grad: grad + auto_reg_grad)
 
-        lat = self.lat_dropout(lat)
+        if lat.requires_grad:
+            self.lat_center.data.copy_(0.9 * self.lat_center + 0.1 * lat.mean(dim=0, keepdim=True))
+
+        lat = lat - self.lat_center
         score = self.lat_to_score(lat)
 
         return score
