6f2ca7790466e930b93c4da83766f21ca8df182f
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-05-24 17:00:41 +0200

    testing... testing...

--- src/networks/base.py
+++ src/networks/base.py
@@ -29,7 +29,7 @@ class Discriminator(nn.Module):
         self.auto_reg = auto_reg
 
         self.register_buffer('lat_proj', torch.randn(16, self.lat_size, self.lat_size))
-        self.register_buffer('lat_bias', torch.randn(16, self.lat_size))
+        # self.register_buffer('lat_bias', torch.randn(16, self.lat_size))
         self.register_buffer('embedding_mat', torch.eye(n_labels))
         self.exp_yembed = nn.Linear(n_labels, self.lat_size, bias=False)
         self.dyna_lat_to_score = DynaLinear(self.lat_size, self.lat_size * 2, 1, bias=False)
@@ -50,7 +50,7 @@ class Discriminator(nn.Module):
                 auto_reg_grad = (2e-3 / lat.numel()) * lat
             lat.register_hook(lambda grad: grad + auto_reg_grad)
 
-        lat = torch.addmm(torch.cat([self.lat_bias] * (lat.size(0) // 16), 0), lat, torch.cat([self.lat_proj] * (lat.size(0) // 16)))
+        lat = torch.bmm(lat, torch.cat([self.lat_proj] * (lat.size(0) // 16))) # + torch.cat([self.lat_bias] * (lat.size(0) // 16), 0)
         score = (self.lat_to_score(lat) * yembed).sum(dim=1, keepdim=True) * (1 / np.sqrt(yembed.shape[1]))
         score = score + self.dyna_lat_to_score(lat, self.exp_yembed(yembed))
 
@@ -130,7 +130,7 @@ class UnconditionalDiscriminator(nn.Module):
         self.auto_reg = auto_reg
 
         self.register_buffer('lat_proj', torch.randn(16, self.lat_size, self.lat_size))
-        self.register_buffer('lat_bias', torch.randn(16, self.lat_size))
+        # self.register_buffer('lat_bias', torch.randn(16, self.lat_size))
         self.lat_to_score = nn.Linear(self.lat_size, 1, bias=False)
 
     def forward(self, lat):
@@ -139,7 +139,7 @@ class UnconditionalDiscriminator(nn.Module):
                 auto_reg_grad = (2e-3 / lat.numel()) * lat
             lat.register_hook(lambda grad: grad + auto_reg_grad)
 
-        lat = torch.addbmm(torch.cat([self.lat_bias] * (lat.size(0) // 16), 0), lat, torch.cat([self.lat_proj] * (lat.size(0) // 16)))
+        lat = torch.bmm(lat, torch.cat([self.lat_proj] * (lat.size(0) // 16))) # + torch.cat([self.lat_bias] * (lat.size(0) // 16), 0)
         score = self.lat_to_score(lat)
 
         return score
