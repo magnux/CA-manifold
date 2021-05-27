790ddc2977a3b3052cb0932a9dac8833884b688c
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-05-27 15:52:29 +0200

    testing... testing...

--- src/networks/base.py
+++ src/networks/base.py
@@ -3,7 +3,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 from src.layers.residualblock import ResidualBlock
 from src.layers.linearresidualblock import LinearResidualBlock
-from src.layers.linearresidualmemory import LinearResidualMemory
 from src.layers.irm import IRMLinear
 from src.layers.dynalinear import DynaLinear
 from src.layers.augment.augment import AugmentPipe, augpipe_specs
@@ -31,7 +30,6 @@ class Discriminator(nn.Module):
         self.register_buffer('embedding_mat', torch.eye(n_labels))
         self.exp_yembed = nn.Linear(n_labels, self.lat_size, bias=False)
         self.dyna_lat_to_score = DynaLinear(self.lat_size, self.lat_size * 2, 1, bias=False)
-        self.lat_mem = LinearResidualMemory(self.lat_size)
         self.lat_to_score = nn.Linear(self.lat_size, n_labels, bias=False)
 
     def forward(self, lat, y):
@@ -44,7 +42,6 @@ class Discriminator(nn.Module):
         else:
             yembed = y
 
-        lat = self.lat_mem(lat)
         score = (self.lat_to_score(lat) * yembed).sum(dim=1, keepdim=True) * (1 / np.sqrt(yembed.shape[1]))
         score = score + self.dyna_lat_to_score(lat, self.exp_yembed(yembed))
 
@@ -68,7 +65,6 @@ class Generator(nn.Module):
         self.dyna_z_to_lat = DynaLinear(self.embed_size, self.z_dim, self.lat_size, bias=False)
         self.z_irm = IRMLinear(self.z_dim, 3)
         self.z_to_lat = nn.Linear(self.z_dim + self.embed_size, self.lat_size, bias=False)
-        self.lat_mem = LinearResidualMemory(self.lat_size)
 
     def forward(self, z, y):
         assert (z.size(0) == y.size(0))
@@ -87,7 +83,6 @@ class Generator(nn.Module):
         z = self.z_irm(z)
         lat = self.z_to_lat(torch.cat([z, yembed], dim=1))
         lat = lat + self.dyna_z_to_lat(z, yembed)
-        lat = self.lat_mem(lat)
 
         return lat
 
@@ -123,12 +118,12 @@ class UnconditionalDiscriminator(nn.Module):
     def __init__(self, lat_size, **kwargs):
         super().__init__()
         self.lat_size = lat_size
-        self.lat_mem = LinearResidualMemory(self.lat_size)
+        self.dyna_lat_to_score = DynaLinear(self.lat_size, self.lat_size, 1, bias=False)
         self.lat_to_score = nn.Linear(self.lat_size, 1, bias=False)
 
     def forward(self, lat):
-        lat = self.lat_mem(lat)
         score = self.lat_to_score(lat)
+        score = score + self.dyna_lat_to_score(lat, lat)
 
         return score
 
