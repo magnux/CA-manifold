0960218634c7019eff7670d0940315fbab2996f1
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-06-03 18:58:57 +0200

    testing... testing...

--- src/networks/base.py
+++ src/networks/base.py
@@ -4,6 +4,7 @@ import torch.nn.functional as F
 from src.layers.residualblock import ResidualBlock
 from src.layers.linearresidualblock import LinearResidualBlock
 from src.layers.irm import IRMLinear
+from src.layers.dynalinear import DynaLinear
 from src.layers.augment.augment import AugmentPipe, augpipe_specs
 from src.utils.loss_utils import vae_sample_gaussian, vae_gaussian_kl_loss
 import numpy as np
@@ -27,6 +28,8 @@ class Discriminator(nn.Module):
         self.embed_size = embed_size
 
         self.register_buffer('embedding_mat', torch.eye(n_labels))
+        self.exp_yembed = nn.Linear(n_labels, self.embed_size, bias=False)
+        self.dyna_lat_to_score = DynaLinear(self.embed_size, self.lat_size, 1, bias=False)
         self.lat_to_score = nn.Linear(self.lat_size, n_labels, bias=False)
 
     def forward(self, lat, y):
@@ -40,12 +43,13 @@ class Discriminator(nn.Module):
             yembed = y
 
         score = (self.lat_to_score(lat) * yembed).sum(dim=1, keepdim=True) * (1 / np.sqrt(yembed.shape[1]))
+        score = score + self.dyna_lat_to_score(lat, self.exp_yembed(yembed))
 
         return score
 
 
 class Generator(nn.Module):
-    def __init__(self, n_labels, lat_size, z_dim, embed_size, norm_z=True, **kwargs):
+    def __init__(self, n_labels, lat_size, z_dim, embed_size, norm_z=False, **kwargs):
         super().__init__()
         self.lat_size = lat_size
         self.fhidden = lat_size if lat_size > 3 else 512
@@ -54,8 +58,9 @@ class Generator(nn.Module):
         self.norm_z = norm_z
 
         self.register_buffer('embedding_mat', torch.eye(n_labels))
-        self.proj_yembed = nn.Linear(n_labels, self.embed_size)
-        self.z_to_lat = LinearResidualBlock(self.z_dim + self.embed_size, self.lat_size)
+        self.exp_yembed = nn.Linear(n_labels, self.embed_size, bias=False)
+        self.dyna_z_to_lat = DynaLinear(self.embed_size, self.z_dim, self.lat_size, bias=False)
+        self.z_to_lat = nn.Linear(self.z_dim + self.embed_size, self.lat_size, bias=False)
 
     def forward(self, z, y):
         assert (z.size(0) == y.size(0))
@@ -70,8 +75,9 @@ class Generator(nn.Module):
         if self.norm_z:
             z = F.normalize(z, dim=1)
 
-        yembed = self.proj_yembed(yembed)
+        yembed = self.exp_yembed(yembed)
         lat = self.z_to_lat(torch.cat([z, yembed], dim=1))
+        lat = lat + self.dyna_z_to_lat(z, yembed)
 
         return lat
 
@@ -83,7 +89,7 @@ class LabsEncoder(nn.Module):
         self.embed_size = embed_size
         self.register_buffer('embedding_mat', torch.eye(n_labels))
 
-        self.yembed_to_lat = LinearResidualBlock(n_labels, self.lat_size)
+        self.yembed_to_lat = nn.Linear(n_labels, self.lat_size, bias=False)
 
     def forward(self, y):
         if y.dtype is torch.int64:
@@ -112,13 +118,13 @@ class UnconditionalDiscriminator(nn.Module):
 
 
 class UnconditionalGenerator(nn.Module):
-    def __init__(self, lat_size, z_dim, norm_z=True, **kwargs):
+    def __init__(self, lat_size, z_dim, norm_z=False, **kwargs):
         super().__init__()
         self.lat_size = lat_size
         self.z_dim = z_dim
         self.norm_z = norm_z
 
-        self.z_to_lat = LinearResidualBlock(self.z_dim, self.lat_size)
+        self.z_to_lat = nn.Linear(self.z_dim, self.lat_size, bias=False)
 
     def forward(self, z):
         if self.norm_z:
