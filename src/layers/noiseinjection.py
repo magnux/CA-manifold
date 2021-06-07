97ab9f6506c02f102f3ffb306e5442afb8131f29
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-06-07 10:48:29 +0200

    testing... testing...

--- src/layers/noiseinjection.py
+++ src/layers/noiseinjection.py
@@ -7,19 +7,17 @@ from src.layers.equallinear import EqualLinear
 class NoiseInjection(nn.Module):
     def __init__(self, fin):
         super().__init__()
-        self.fin = fin
-        self.noise_weights = nn.Parameter(torch.randn(1, self.fin))
+        self.lat_to_fin = nn.Linear(1, fin)
 
     def forward(self, x, noise=None):
-        if noise is None:
-            noise_shape = [int(s) for s in x.shape]
-            if x.dim() > 2:
-                noise_shape[1] = 1
-            noise = torch.randn(noise_shape, device=x.device)
+        batch_size = x.size(0)
+        in_size = x.size(2)
+        x_dim = x.dim() - 2
 
-        exp_weights = self.noise_weights
-        if x.dim() > 2:
-            exp_weights = exp_weights.view(*[1, self.fin] + [1 for _ in x.shape[2:]])
-        x_new = x + exp_weights * noise
+        if noise is None:
+            noise = torch.randn([batch_size] + [in_size for _ in range(x_dim)] + [1], device=x.device)
+        noise = self.lat_to_fin(noise).permute(*[0, x_dim+1] + [i for i in range(2, x_dim+1)] + [1])
+
+        x_new = x + noise
 
         return x_new
