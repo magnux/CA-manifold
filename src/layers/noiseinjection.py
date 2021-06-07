9b6c79b1b2e6d61062bf53c4526c9908e68a0925
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-06-07 10:12:24 +0200

    testing... testing...

--- src/layers/noiseinjection.py
+++ src/layers/noiseinjection.py
@@ -8,7 +8,7 @@ class NoiseInjection(nn.Module):
     def __init__(self, fin):
         super().__init__()
         self.fin = fin
-        self.noise_weights = nn.Parameter(torch.zeros(1, self.fin))
+        self.noise_weights = nn.Parameter(torch.randn(1, self.fin))
 
     def forward(self, x, noise=None):
         if noise is None:
