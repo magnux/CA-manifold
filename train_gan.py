fdd50a414992dc7cfa827aee64a1ec0a274bcd16
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-06-16 15:58:03 +0200

    testing... testing...

--- train_gan.py
+++ train_gan.py
@@ -212,6 +212,7 @@ for epoch in range(model_manager.start_epoch, config['training']['n_epochs']):
                                                                                     reg_dis_mean, reg_dis_target, loss_dis_mean)
 
                     g_factor_enc, g_factor_dec = update_g_factors(g_factor_enc, g_factor_dec, labs_dis_enc_sign, labs_dis_dec_sign, sign_mean_target)
+                    model_manager.set_n_calls('dis_encoder', max(1, int(0.5 * (g_factor_enc + g_factor_dec) * n_calls)))
                     # reg_dis_target = config['training']['lr'] * ((0.5 * (g_factor_enc + g_factor_dec)) ** 4)
                     # dis_encoder.fire_rate = 0.5 * (g_factor_enc + g_factor_dec)
                     # grad_mult(dis_encoder, 0.5 * (g_factor_enc + g_factor_dec))
