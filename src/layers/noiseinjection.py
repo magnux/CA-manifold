59d8a1af0490b3625e2770f539810eb87184c447
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-06-20 10:50:28 +0200

    testing... testing...

--- src/layers/noiseinjection.py
+++ src/layers/noiseinjection.py
@@ -6,22 +6,23 @@ from src.layers.linearresidualblock import LinearResidualBlock
 class NoiseInjection(nn.Module):
     def __init__(self, fin):
         super().__init__()
-        self.lat_to_fin = LinearResidualBlock(1, fin)
+        self.fin = fin
+        self.lat_to_fin = LinearResidualBlock(fin, fin)
 
     def forward(self, x, noise=None):
-        batch_size = x.size(0)
+        batch_size = x.shape[0]
 
         squeeze_x = False
         if x.dim() == 2:
             x = x.unsqueeze(2)
             squeeze_x = True
 
-        in_size = x.size(2)
+        sp_size = [int(i) for i in x.shape[2:]]
         x_dim = x.dim() - 2
 
         if noise is None:
-            noise = torch.randn([batch_size] + [in_size for _ in range(x_dim)] + [1], device=x.device)
-        noise = self.lat_to_fin(noise).permute(*[0, x_dim+1] + [i for i in range(2, x_dim+1)] + [1])
+            noise = torch.randn([batch_size] + sp_size + [self.fin], device=x.device)
+        noise = self.lat_to_fin(noise).permute(*[0, x_dim+1] + [i for i in range(1, x_dim+1)])
 
         x_new = x + noise
 
