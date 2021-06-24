72f6a2e2ebabbe4794fa6d0619e2e35698398501
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-06-24 05:45:25 +0200

    testing... testing...

--- src/networks/base.py
+++ src/networks/base.py
@@ -6,6 +6,7 @@ from src.layers.linearresidualblock import LinearResidualBlock
 from src.layers.irm import IRMLinear
 from src.layers.augment.augment import AugmentPipe, augpipe_specs
 from src.utils.loss_utils import vae_sample_gaussian, vae_gaussian_kl_loss
+from src.layers.expscale import ExpScale
 
 
 class Classifier(nn.Module):
@@ -53,15 +54,15 @@ class Generator(nn.Module):
         self.norm_z = norm_z
 
         self.register_buffer('embedding_mat', torch.eye(n_labels))
-        self.yembed_fc = nn.Linear(n_labels, self.embed_size)
-        # self.yembed_irm = nn.Sequential(
-        #     nn.Linear(n_labels, self.embed_size),
-        #     IRMLinear(self.embed_size, exp_scale=True)
-        # )
-        # self.z_irm = IRMLinear(self.z_dim, exp_scale=True)
+        self.yembed_irm = nn.Sequential(
+            nn.Linear(n_labels, self.embed_size),
+            IRMLinear(self.embed_size, exp_scale=True)
+        )
+        self.z_irm = IRMLinear(self.z_dim, exp_scale=True)
         self.z_to_lat = nn.Sequential(
             nn.Linear(self.z_dim + self.embed_size, self.lat_size, bias=False),
             LinearResidualBlock(self.lat_size, self.lat_size),
+            ExpScale(self.lat_size),
             LinearResidualBlock(self.lat_size, self.lat_size),
         )
 
@@ -78,9 +79,8 @@ class Generator(nn.Module):
         if self.norm_z:
             z = F.normalize(z, dim=1)
 
-        # yembed = self.yembed_irm(yembed)
-        # z = self.z_irm(z)
-        yembed = self.yembed_fc(yembed)
+        yembed = self.yembed_irm(yembed)
+        z = self.z_irm(z)
         lat = self.z_to_lat(torch.cat([z, yembed], dim=1))
 
         return lat
@@ -93,11 +93,10 @@ class LabsEncoder(nn.Module):
         self.embed_size = embed_size
         self.register_buffer('embedding_mat', torch.eye(n_labels))
 
-        self.yembed_fc = nn.Linear(n_labels, self.embed_size)
-        # self.yembed_irm = nn.Sequential(
-        #     nn.Linear(n_labels, self.embed_size),
-        #     IRMLinear(self.embed_size, exp_scale=True),
-        # )
+        self.yembed_irm = nn.Sequential(
+            nn.Linear(n_labels, self.embed_size),
+            IRMLinear(self.embed_size, exp_scale=True),
+        )
         self.yembed_to_lat = nn.Linear(self.embed_size, self.lat_size, bias=False)
 
     def forward(self, y):
@@ -109,8 +108,7 @@ class LabsEncoder(nn.Module):
         else:
             yembed = y
 
-        # yembed = self.yembed_irm(yembed)
-        yembed = self.yembed_fc(yembed)
+        yembed = self.yembed_irm(yembed)
         lat = self.yembed_to_lat(yembed)
 
         return lat
@@ -135,14 +133,14 @@ class UnconditionalGenerator(nn.Module):
         self.z_dim = z_dim
         self.norm_z = norm_z
 
-        # self.z_irm = IRMLinear(self.z_dim, 3, exp_scale=True)
+        self.z_irm = IRMLinear(self.z_dim, 3, exp_scale=True)
         self.z_to_lat = nn.Linear(self.z_dim, self.lat_size, bias=False)
 
     def forward(self, z):
         if self.norm_z:
             z = F.normalize(z, dim=1)
 
-        # z = self.z_irm(z)
+        z = self.z_irm(z)
         lat = self.z_to_lat(z)
 
         return lat
