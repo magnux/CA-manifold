7a69c1df3877d29502f9295231282ba32c622e3c
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-06-29 12:24:29 +0200

    testing... testing...

--- src/layers/projscale.py
+++ src/layers/projscale.py
@@ -5,7 +5,7 @@ import torch.nn as nn
 class ProjScale(nn.Module):
     def __init__(self, fin):
         super(ProjScale, self).__init__()
-        self.proj_weight = nn.Parameter(torch.randn(fin, fin))
+        self.proj_weight = nn.Parameter(torch.randn(1, fin, fin))
 
     def forward(self, x):
-        return torch.bmm(x.view(x.shape[0], 1, x.shape[1]), self.proj_weight).squeeze_(1)
+        return torch.bmm(x.view(x.shape[0], 1, x.shape[1]), self.proj_weight.repeat(x.shape[0], 1, 1)).squeeze_(1)
