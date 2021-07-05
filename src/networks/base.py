aa5dcb3296ce4b8dfb0ba6f915325b7825e720f1
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-07-05 15:37:10 +0200

    testing... testing...

--- src/networks/base.py
+++ src/networks/base.py
@@ -57,7 +57,8 @@ class Generator(nn.Module):
         self.register_buffer('embedding_mat', torch.eye(n_labels))
         self.labs_to_yembed = nn.Linear(n_labels, self.embed_size)
         self.yembed_to_lat = nn.Linear(self.embed_size, self.lat_size, bias=False)
-        self.yembed_cond_z = nn.Linear(self.embed_size, self.z_dim * self.lat_size, bias=False)
+
+        self.z_cond_yembed = nn.Linear(self.z_dim, self.embed_size * self.lat_size, bias=False)
 
     def forward(self, z, y):
         assert (z.size(0) == y.size(0))
@@ -76,8 +77,8 @@ class Generator(nn.Module):
         yembed = self.labs_to_yembed(yembed)
         lat = self.yembed_to_lat(yembed)
 
-        yembed_cond = self.yembed_cond_z(yembed).reshape(batch_size, self.z_dim, self.lat_size)
-        lat = lat + torch.bmm(z.reshape(batch_size, 1, self.z_dim), yembed_cond).squeeze(1) / (self.lat_size ** 0.5)
+        z_cond = self.z_cond_yembed(z).reshape(batch_size, self.embed_size, self.lat_size)
+        lat = lat + torch.bmm(yembed.reshape(batch_size, 1, self.embed_size), z_cond).squeeze(1)
 
         return lat
 
