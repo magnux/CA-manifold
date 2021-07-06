fac9309779711ba42f8223f5b5e07d24f5ab910c
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-07-06 11:28:42 +0200

    testing... testing...

--- src/networks/base.py
+++ src/networks/base.py
@@ -3,7 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 from src.layers.residualblock import ResidualBlock
 from src.layers.linearresidualblock import LinearResidualBlock
-from src.layers.expscale import ExpScale
+from src.layers.dynalinear import DynaLinear
 from src.layers.irm import IRMLinear
 from src.layers.augment.augment import AugmentPipe, augpipe_specs
 from src.utils.loss_utils import vae_sample_gaussian, vae_gaussian_kl_loss
@@ -57,17 +57,11 @@ class Generator(nn.Module):
         self.n_calls = n_calls
 
         self.register_buffer('embedding_mat', torch.eye(n_labels))
-        self.labs_to_yembed = nn.Linear(n_labels, self.embed_size)
-        self.z_to_lat = nn.Linear(self.z_dim + self.embed_size, self.lat_size, bias=False)
-        self.lat_trans = nn.Sequential(
-            ExpScale(self.lat_size),
-            LinearResidualBlock(self.lat_size, self.lat_size),
-            LinearResidualBlock(self.lat_size, self.lat_size),
-        )
+        self.z_to_lat = nn.Linear(self.z_dim, self.lat_size, bias=False)
+        self.lat_trans = DynaLinear(n_labels, self.lat_size, self.lat_size)
 
     def forward(self, z, y):
         assert (z.size(0) == y.size(0))
-        batch_size = z.shape[0]
         if y.dtype is torch.int64:
             if y.dim() == 1:
                 yembed = self.embedding_mat[y]
@@ -79,11 +73,10 @@ class Generator(nn.Module):
         if self.norm_z:
             z = F.normalize(z, dim=1)
 
-        yembed = self.labs_to_yembed(yembed)
-        lat = self.z_to_lat(torch.cat([z, yembed], dim=1))
+        lat = self.z_to_lat(z)
 
         for _ in range(self.n_calls):
-            lat = lat + 0.1 * self.lat_trans(lat)
+            lat = lat + 0.1 * self.lat_trans(lat, yembed)
 
         return lat
 
@@ -134,20 +127,12 @@ class UnconditionalGenerator(nn.Module):
         self.n_calls = n_calls
 
         self.z_to_lat = nn.Linear(self.z_dim, self.lat_size, bias=False)
-        self.lat_trans = nn.Sequential(
-            ExpScale(self.lat_size),
-            LinearResidualBlock(self.lat_size, self.lat_size),
-            LinearResidualBlock(self.lat_size, self.lat_size),
-        )
 
     def forward(self, z):
         if self.norm_z:
             z = F.normalize(z, dim=1)
 
         lat = self.z_to_lat(z)
-
-        for _ in range(self.n_calls):
-            lat = lat + 0.1 * self.lat_trans(lat)
 
         return lat
 
