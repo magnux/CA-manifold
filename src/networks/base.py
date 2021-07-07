8d6a7d22e1803bae6195c203f8b61ff76fb86634
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-07-07 09:46:10 +0200

    testing... testing...

--- src/networks/base.py
+++ src/networks/base.py
@@ -28,7 +28,7 @@ class Discriminator(nn.Module):
         self.embed_size = embed_size
 
         self.register_buffer('embedding_mat', torch.eye(n_labels))
-        self.labs_to_yembed = nn.Linear(n_labels, self.embed_size)
+        self.labs_to_yembed = QLinear(n_labels, self.embed_size)
         self.lat_to_score = nn.Linear(self.lat_size, n_labels, bias=False)
 
     def forward(self, lat, y):
@@ -58,11 +58,10 @@ class Generator(nn.Module):
         self.n_calls = n_calls
 
         self.register_buffer('embedding_mat', torch.eye(n_labels))
-        self.labs_to_yembed = nn.Linear(n_labels, self.embed_size)
+        self.labs_to_yembed = QLinear(n_labels, self.embed_size)
         self.yembed_to_lat = nn.Linear(self.embed_size, self.lat_size, bias=False)
-        self.z_frac_quant = QLinear(self.z_dim, self.z_dim)
-        self.z_frac_block = DynaLinearResidualBlock(self.embed_size, self.z_dim, self.z_dim, self.z_dim)
-        self.z_to_lat = nn.Linear(self.z_dim, self.lat_size, bias=False)
+        self.z_quant = QLinear(self.z_dim, self.z_dim, bias=False)
+        self.z_to_lat = DynaLinearResidualBlock(self.embed_size, self.z_dim, self.lat_size, self.z_dim)
 
     def forward(self, z, y):
         assert (z.size(0) == y.size(0))
@@ -80,12 +79,8 @@ class Generator(nn.Module):
         yembed = self.labs_to_yembed(yembed)
         lat = self.yembed_to_lat(yembed)
 
-        for _ in range(self.n_calls):
-            z_new = self.z_frac_quant(z)
-            z_new = self.z_frac_block(z_new, yembed)
-            z = z + 0.1 * z_new
-
-        lat = lat + self.z_to_lat(z)
+        z = self.z_quant(z)
+        lat = lat + self.z_to_lat(z, yembed)
 
         return lat
 
@@ -97,7 +92,7 @@ class LabsEncoder(nn.Module):
         self.embed_size = embed_size
         self.register_buffer('embedding_mat', torch.eye(n_labels))
 
-        self.labs_to_yembed = nn.Linear(n_labels, self.embed_size)
+        self.labs_to_yembed = QLinear(n_labels, self.embed_size)
         self.yembed_to_lat = nn.Linear(self.embed_size, self.lat_size, bias=False)
 
     def forward(self, y):
@@ -135,12 +130,14 @@ class UnconditionalGenerator(nn.Module):
         self.norm_z = norm_z
         self.n_calls = n_calls
 
+        self.z_quant = QLinear(self.z_dim, self.z_dim, bias=False)
         self.z_to_lat = nn.Linear(self.z_dim, self.lat_size, bias=False)
 
     def forward(self, z):
         if self.norm_z:
             z = F.normalize(z, dim=1)
 
+        z = self.z_quant(z)
         lat = self.z_to_lat(z)
 
         return lat
