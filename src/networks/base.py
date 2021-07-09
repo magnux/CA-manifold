165ca067bb564001a4aa33f7a7db5b079a72b8dd
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-07-09 08:55:00 +0200

    testing... testing...

--- src/networks/base.py
+++ src/networks/base.py
@@ -61,7 +61,8 @@ class Generator(nn.Module):
         self.yembed_to_lat = nn.Linear(self.embed_size, self.lat_size, bias=False)
 
         self.z_irm = IRMLinear(self.z_dim)
-        self.z_cond = DynaLinear(self.embed_size, self.z_dim, self.z_dim, bias=False)
+        self.z_cond0 = DynaLinear(self.embed_size, self.z_dim, self.z_dim // 2, bias=False)
+        self.z_cond1 = DynaLinear(self.embed_size, self.z_dim // 2, self.z_dim, bias=False)
         self.z_to_lat = nn.Linear(self.z_dim, self.lat_size, bias=False)
 
     def forward(self, z, y):
@@ -81,7 +82,8 @@ class Generator(nn.Module):
         lat = self.yembed_to_lat(yembed)
 
         z = self.z_irm(z)
-        z = self.z_cond(z, yembed)
+        z = self.z_cond0(z, yembed)
+        z = self.z_cond1(z, yembed)
         lat = lat + self.z_to_lat(z)
 
         return lat
