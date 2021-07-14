14324c2a5420c9baba1bb3f98e8182ee311207c6
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-07-15 00:15:54 +0200

    testing... testing...

--- src/networks/cell_ae.py
+++ src/networks/cell_ae.py
@@ -61,7 +61,7 @@ class InjectedEncoder(nn.Module):
             self.skip_fire_mask = torch.tensor(np.indices((1, 1, self.image_size + (2 if self.causal else 0), self.image_size + (2 if self.causal else 0))).sum(axis=0) % 2, requires_grad=False)
 
         self.out_conv = nn.Conv2d(self.n_filter, sum(self.split_sizes), 1, 1, 0)
-        self.out_to_lat = LinearResidualBlock(sum(self.conv_state_size), lat_size if not z_out else z_dim)
+        self.out_to_lat = nn.Linear(sum(self.conv_state_size), lat_size if not z_out else z_dim)
 
     def forward(self, x, inj_lat=None):
         assert (inj_lat is not None) == self.injected, 'latent should only be passed to injected encoders'
