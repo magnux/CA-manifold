a327f9920b9478fd23a654f467fabeb756c7b467
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-07-15 14:39:51 +0200

    testing... testing...

--- src/layers/channeldither.py
+++ src/layers/channeldither.py
@@ -5,7 +5,8 @@ import numpy as np
 
 
 def get_dither_kernel_1d(channels):
-    dither_kernel = torch.tensor([0., 0., 7/16], dtype=torch.float32).view(1, 1, 3)
+    dither_kernel = 1e-2 * torch.randn((1, 1, 3), dtype=torch.float32)
+    dither_kernel[:, :, 1] = 0.
 
     dither_kernel = dither_kernel.repeat(channels, 1, 1)
 
@@ -13,9 +14,8 @@ def get_dither_kernel_1d(channels):
 
 
 def get_dither_kernel_2d(channels):
-    dither_kernel = torch.tensor([[0.,   0.,   0.],
-                                  [0.,   0.,   7/16],
-                                  [3/16, 5/16, 1/16]], dtype=torch.float32).view(1, 1, 3, 3)
+    dither_kernel = 1e-2 * torch.randn((1, 1, 3, 3), dtype=torch.float32)
+    dither_kernel[:, :, 1, 1] = 0.
 
     dither_kernel = dither_kernel.repeat(channels, 1, 1, 1)
 
@@ -41,5 +41,36 @@ class ChannelDither(nn.Module):
         self.c_factor = 1 + dim
 
     def forward(self, x):
-        x_dither = self.conv(F.relu(-x), weight=self.weight, stride=1, padding=1, groups=self.groups)
-        return x - x_dither
+        x_dither = self.conv(x, weight=self.weight, stride=1, padding=1, groups=self.groups)
+        return x + x_dither
+
+
+if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+
+
+    def close_event():
+        plt.close()
+
+    fig = plt.figure()
+    timer = fig.canvas.new_timer(interval=500)
+    timer.add_callback(close_event)
+
+    c_size = 64
+    # c_size = 128
+
+    canvas = torch.Tensor(np.sum(np.indices((c_size, c_size)), axis=0)).reshape(1, 1, c_size, c_size)
+    canvas = (2 * (canvas/ canvas.max())) - 1
+
+    plt.imshow(canvas.view(c_size, c_size))
+    plt.show()
+
+    plt.imshow(F.relu(canvas).view(c_size, c_size))
+    plt.show()
+
+    cd = ChannelDither(1)
+    plt.imshow(cd(canvas).view(c_size, c_size) - canvas.view(c_size, c_size))
+    plt.show()
+
+    dit_diff = cd(canvas) - canvas
+    print(dit_diff.mean(), dit_diff.std(), dit_diff.max(), dit_diff.min())
