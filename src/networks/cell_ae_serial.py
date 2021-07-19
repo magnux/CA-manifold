7c660be7e482c338e48db6fba59350e181ff5eb6
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-07-19 09:54:13 +0200

    testing... testing...

--- src/networks/cell_ae_serial.py
+++ src/networks/cell_ae_serial.py
@@ -27,7 +27,7 @@ class InjectedEncoder(nn.Module):
         self.in_chan = channels
         self.n_filter = n_filter
         self.lat_size = lat_size if lat_size > 3 else 512
-        self.n_layers = int(np.ceil(np.log2(image_size)))
+        self.n_layers = int(np.ceil(np.log2(image_size)) - 1)
         self.n_calls = n_calls
         self.perception_noise = perception_noise
         self.fire_rate = fire_rate
@@ -113,7 +113,7 @@ class InjectedEncoder(nn.Module):
                         out_new = out_new * self.skip_fire_mask.to(device=x.device).to(float_type)
                     else:
                         out_new = out_new * (1 - self.skip_fire_mask.to(device=x.device).to(float_type))
-                out = out + (leak_factor * out_new)
+                out = out_new
                 if self.causal:
                     out = out[:, :, 1:, 1:]
                 if self.auto_reg and out.requires_grad:
@@ -162,7 +162,7 @@ class Decoder(nn.Module):
         self.image_size = image_size
         self.n_filter = n_filter
         self.lat_size = lat_size
-        self.n_layers = int(np.ceil(np.log2(image_size)))
+        self.n_layers = int(np.ceil(np.log2(image_size)) - 1)
         self.n_calls = n_calls
         self.perception_noise = perception_noise
         self.fire_rate = fire_rate
@@ -274,7 +274,7 @@ class Decoder(nn.Module):
                     else:
                         out_new = out_new * (1 - self.skip_fire_mask.to(device=lat.device).to(float_type))
                 out_new = self.frac_noise[l](out_new)
-                out = out + (leak_factor * out_new)
+                out = out_new
                 if self.causal:
                     out = out[:, :, 1:, 1:]
                 if self.auto_reg and out.requires_grad:
