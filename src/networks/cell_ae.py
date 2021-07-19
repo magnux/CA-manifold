2f46356eb820b0ecb541b4d5bb0653bd81a0c63f
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-07-19 11:58:56 +0200

    testing... testing...

--- src/networks/cell_ae.py
+++ src/networks/cell_ae.py
@@ -102,7 +102,7 @@ class InjectedEncoder(nn.Module):
                     out_new = out_new * self.skip_fire_mask.to(device=x.device).to(float_type)
                 else:
                     out_new = out_new * (1 - self.skip_fire_mask.to(device=x.device).to(float_type))
-            out = out_new
+            out = 0.1 * out_new
             if self.causal:
                 out = out[:, :, 1:, 1:]
             if self.auto_reg and out.requires_grad:
@@ -250,7 +250,7 @@ class Decoder(nn.Module):
                 else:
                     out_new = out_new * (1 - self.skip_fire_mask.to(device=lat.device).to(float_type))
             out_new = self.frac_noise(out_new)
-            out = out_new
+            out = 0.1 * out_new
             if self.causal:
                 out = out[:, :, 1:, 1:]
             if self.auto_reg and out.requires_grad:
