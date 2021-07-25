f1158a98b91416245ec0cfc0549a0b5a48080476
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-07-25 15:45:18 +0200

    testing... testing...

--- src/networks/shade_ae.py
+++ src/networks/shade_ae.py
@@ -118,7 +118,7 @@ class Decoder(nn.Module):
 
         self.in_proj = nn.Parameter(torch.nn.init.orthogonal_(torch.empty(1, self.n_filter)).reshape(1, self.n_filter, 1, 1))
 
-        self.seed = nn.Parameter(torch.nn.init.orthogonal_(torch.empty(1, self.n_filter)).unsqueeze(2).unsqueeze(3).repeat(1, 1, self.image_size, self.image_size))
+        self.seed = nn.Parameter(ca_seed(1, self.n_filter, self.image_size, 'cpu', all_channels=True))
 
         self.frac_conv = ResidualBlock(self.n_filter, self.n_filter, self.n_filter * 4, 1, 1, 0)
         self.frac_sobel = SinSobel(self.n_filter, [(2 ** i) + 1 for i in range(1, int(np.log2(image_size)-1), 1)],
