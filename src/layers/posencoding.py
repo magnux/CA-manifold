26fd151bf4e8593ea3f54786a201bfc7ccd2764c
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-07-29 15:34:15 +0200

    testing... testing...

--- src/layers/posencoding.py
+++ src/layers/posencoding.py
@@ -114,7 +114,7 @@ class LatFreqEncoding(nn.Module):
         super(LatFreqEncoding, self).__init__()
         self.lat_size = lat_size
         self.norm = norm
-        cos_freq_encoding = sin_cos_pos_encoding_1d(self.lat_size, 1, 8)
+        cos_freq_encoding = sin_cos_pos_encoding_1d(self.lat_size, 1)
         self.register_buffer('sin_cos_pos_encoding_1d', cos_freq_encoding)
         self.to_freq_size = nn.Linear(self.lat_size, cos_freq_encoding.size(1), bias=False)
 
@@ -129,7 +129,7 @@ class LatFreqEncoding(nn.Module):
 
 
 class ConvFreqDecoder(nn.Module):
-    def __init__(self, size, dim=2):
+    def __init__(self, n_filter, size, dim=2):
         super(ConvFreqDecoder, self).__init__()
         self.register_buffer('freq_decoder', sin_cos_pos_encoding_nd(size, dim))
 
@@ -142,7 +142,7 @@ class ConvFreqDecoder(nn.Module):
         else:
             raise RuntimeError('Only 1, 2 and 3 dimensions are supported. Received {}.'.format(dim))
 
-        self.out_conv = self.l_conv(self.n_filter, self.freq_decoder.shape[1], 1, 1, 0)
+        self.out_conv = self.l_conv(n_filter, self.freq_decoder.shape[1], 1, 1, 0)
 
     def forward(self, x):
         return self.out_conv(x) * self.freq_decoder
