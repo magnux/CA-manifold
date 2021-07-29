f8e819a5105b0b53c59baa5241e27750ac33d47f
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-07-29 23:26:10 +0200

    testing... testing...

--- src/networks/cell_ae.py
+++ src/networks/cell_ae.py
@@ -55,7 +55,7 @@ class InjectedEncoder(nn.Module):
             self.skip_fire_mask = torch.tensor(np.indices((1, 1, self.image_size + (2 if self.causal else 0), self.image_size + (2 if self.causal else 0))).sum(axis=0) % 2, requires_grad=False)
 
         self.out_freq = ConvFreqDecoder(self.n_filter, self.image_size)
-        self.freq_to_lat = LinearResidualBlock(self.lat_size + self.out_freq.size(), self.lat_size)
+        self.freq_to_lat = nn.Linear(self.lat_size + self.out_freq.size(), self.lat_size)
         self.lat_out = nn.Linear(self.lat_size, lat_size if not z_out else z_dim)
 
     def forward(self, x, inj_lat=None):
