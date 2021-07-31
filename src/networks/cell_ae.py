185e5c549efe087a4dcada6bc1399546bda509a5
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-07-31 12:55:12 +0200

    testing... testing...

--- src/networks/cell_ae.py
+++ src/networks/cell_ae.py
@@ -56,8 +56,8 @@ class InjectedEncoder(nn.Module):
             self.skip_fire_mask = torch.tensor(np.indices((1, 1, self.image_size + (2 if self.causal else 0), self.image_size + (2 if self.causal else 0))).sum(axis=0) % 2, requires_grad=False)
 
         self.out_freq = ConvFreqDecoder(self.n_filter, self.image_size)
-        self.register_buffer('calls_freq', sin_cos_pos_encoding_1d(self.n_calls).squeeze(0))
-        self.freq_to_lat = LinearResidualBlock(self.out_freq.size() + self.calls_freq.shape[0], self.lat_size)
+        self.register_buffer('calls_freq', sin_cos_pos_encoding_1d(self.n_calls))
+        self.freq_to_lat = LinearResidualBlock(self.out_freq.size() + self.calls_freq.shape[1], self.lat_size)
         self.lat_out = LinearResidualBlock(self.lat_size, lat_size if not z_out else z_dim)
 
     def forward(self, x, inj_lat=None):
@@ -111,7 +111,7 @@ class InjectedEncoder(nn.Module):
             lat_new = torch.cat([dyna_lat, out.mean((2, 3))], 1) if self.env_feedback else dyna_lat
             dyna_lat = inj_lat + 0.1 * self.frac_lat(lat_new)
 
-            freq = torch.cat([self.out_freq(out).mean(dim=(2, 3)), self.calls_freq[:, c]], 1)
+            freq = torch.cat([self.out_freq(out).mean(dim=(2, 3)), self.calls_freq[:, :, c].repeat(batch_size, 1, 1)], 1)
             lat = lat + 0.1 * self.freq_to_lat(freq)
 
         lat = self.lat_out(lat)
