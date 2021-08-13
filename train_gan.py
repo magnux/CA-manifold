507d758ff26790056fc8d41ede52fcda4f0ee3f3
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-08-13 11:58:49 +0200

    testing... testing...

--- train_gan.py
+++ train_gan.py
@@ -179,9 +179,9 @@ for epoch in range(model_manager.start_epoch, n_epochs):
                             model_manager.loss_backward(reg_dis_enc, nets_to_train, retain_graph=True)
                             reg_dis_enc_sum += reg_dis_enc.item() / d_reg_factor
 
-                            reg_dis_enc = (1 / batch_mult) * d_reg_factor * compute_grad_reg(labs_enc, dis_encoder.inj_lat)
-                            model_manager.loss_backward(reg_dis_enc, nets_to_train, retain_graph=True)
-                            reg_dis_enc_sum += reg_dis_enc.item() / d_reg_factor
+                            # reg_dis_enc = (1 / batch_mult) * d_reg_factor * compute_grad_reg(labs_enc, dis_encoder.inj_lat)
+                            # model_manager.loss_backward(reg_dis_enc, nets_to_train, retain_graph=True)
+                            # reg_dis_enc_sum += reg_dis_enc.item() / d_reg_factor
 
                         loss_dis_enc = (1 / batch_mult) * compute_gan_loss(labs_enc, 1)
                         # labs_enc.register_hook(grad_mult_hook(g_factor_enc))
@@ -202,9 +202,9 @@ for epoch in range(model_manager.start_epoch, n_epochs):
                             model_manager.loss_backward(reg_dis_dec, nets_to_train, retain_graph=True)
                             reg_dis_dec_sum += reg_dis_dec.item() / d_reg_factor
 
-                            reg_dis_dec = (1 / batch_mult) * d_reg_factor * compute_grad_reg(labs_dec, dis_encoder.inj_lat)
-                            model_manager.loss_backward(reg_dis_dec, nets_to_train, retain_graph=True)
-                            reg_dis_dec_sum += reg_dis_dec.item() / d_reg_factor
+                            # reg_dis_dec = (1 / batch_mult) * d_reg_factor * compute_grad_reg(labs_dec, dis_encoder.inj_lat)
+                            # model_manager.loss_backward(reg_dis_dec, nets_to_train, retain_graph=True)
+                            # reg_dis_dec_sum += reg_dis_dec.item() / d_reg_factor
 
                         loss_dis_dec = (1 / batch_mult) * compute_gan_loss(labs_dec, 0)
                         # labs_dec.register_hook(grad_mult_hook(g_factor_dec))
