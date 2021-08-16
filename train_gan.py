dcb26024e83ea27b29c64de0face384f05f8af0e
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-08-16 15:31:33 +0200

    testing... testing...

--- train_gan.py
+++ train_gan.py
@@ -128,7 +128,7 @@ torch.autograd.set_detect_anomaly(True)
 g_factor_enc = model_manager.log_manager.get_last('regs', 'g_factor_enc', 1.)
 g_factor_dec = model_manager.log_manager.get_last('regs', 'g_factor_dec', 1.)
 
-lr_mul = model_manager.log_manager.get_last('regs', 'lr_mul', 0.1)
+lr_mul = model_manager.log_manager.get_last('regs', 'lr_mul', 1.)
 
 for epoch in range(model_manager.start_epoch, n_epochs):
     with model_manager.on_epoch(epoch):
@@ -142,7 +142,7 @@ for epoch in range(model_manager.start_epoch, n_epochs):
         # Discriminator mean sign target
         sign_mean_target = 0.2  # 0.5 * (1. - 0.9 ** (n_epochs / (epoch + 1e-8)))
 
-        lr_mul = 0.1 * (1.0 - 0.99 ** (n_epochs / (epoch + 1e-8)))
+        lr_mul = 1. * (1. - 0.999 ** (n_epochs / (epoch + 1e-8)))
         model_manager.set_lr_mul('generator', lr_mul)
         model_manager.set_lr_mul('discriminator', lr_mul)
         model_manager.set_lr_mul('dis_encoder', lr_mul)
