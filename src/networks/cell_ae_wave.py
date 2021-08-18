f6bc4e45612b000259e55f9734a5a950a8b7b5c9
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-08-18 20:45:24 +0200

    testing... testing...

--- src/networks/cell_ae_wave.py
+++ src/networks/cell_ae_wave.py
@@ -46,6 +46,7 @@ class InjectedEncoder(nn.Module):
         self.conv_state_size = [self.n_filter, self.image_size, self.image_size, self.n_filter * self.image_size, self.n_filter * self.image_size, self.image_size ** 2, 1] if self.multi_cut else [self.n_filter]
 
         self.in_conv = nn.Conv2d(self.in_chan if not self.ce_in else self.in_chan * 256, self.n_filter, 1, 1, 0)
+        nn.init.normal_(self.in_conv.weight)
 
         self.register_buffer('wave', sin_cos_pos_encoding_nd(self.image_size, 2, version=2, pos_scale=self.n_filter))
         self.wave_selector = nn.Conv2d(self.wave.shape[1], self.n_filter, 1, 1, 0, bias=None)
