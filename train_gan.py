210cfff14c294c328061fb775d84741bfb2c1289
Alejandro Hernandez <1824645+magnux@users.noreply.github.com>
2021-09-03 18:28:17 +0200

    testing... testing...

--- train_gan.py
+++ train_gan.py
@@ -234,7 +234,8 @@ for epoch in range(model_manager.start_epoch, n_epochs):
                     # grad_mult(dis_encoder, 0.5 * (g_factor_enc + g_factor_dec))
                     # grad_mult(discriminator, 0.5 * (g_factor_enc + g_factor_dec))
 
-                    grad_ema_update(dis_encoder.labs_encoder)
+                    if not retrain:
+                        grad_ema_update(dis_encoder.labs_encoder)
 
                 # Generator step
                 with model_manager.on_step(['decoder', 'generator']) as nets_to_train:
@@ -259,7 +260,8 @@ for epoch in range(model_manager.start_epoch, n_epochs):
                         model_manager.loss_backward(loss_gen_dec, nets_to_train)
                         loss_gen_dec_sum += loss_gen_dec.item()
 
-                    grad_ema_update(generator)
+                    if not retrain:
+                        grad_ema_update(generator)
 
                     # grad_mult(decoder, (0.5 * (g_factor_enc + g_factor_dec)) ** 0.5)
                     # grad_mult(generator, (0.5 * (g_factor_enc + g_factor_dec)) ** 0.5)
